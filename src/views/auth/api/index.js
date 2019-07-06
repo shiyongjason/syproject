8d7bd851c834a492dbc4f5651311c155fd1857a1
wangqi <wangqi@hosjoy.com>
2019-07-06 11:30:34 +0800

    接口调试

--- src/views/auth/api/index.js
+++ src/views/auth/api/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+// 获取BOSS组织结构树
+export const findBOSSOrganization = () => axios.get('/uaa/department/getDepartmentTree')
 
-
-
+// 查询组织机构人员
+export const findOrganizationEmployee = (params) => axios.post('/uaa/employee/queryListByDeptCode', params)
