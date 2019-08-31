6a2f1850e9f99357dcaa90c0dbbae630c1b65cba
shenyue <327274956@qq.com>
2019-08-31 15:54:10 +0800

    尽调

--- src/views/bestonline/justice/components/copartnerLook.vue
+++ src/views/bestonline/justice/components/copartnerLook.vue
@@ -1,72 +1,52 @@
 <template>
-<el-collapse-item name="2">
-    <template slot="title">
-        <p class="title-p">合伙人信息</p>
-    </template>
-    <template v-if="copartnerInfoList && copartnerInfoList.length > 0">
-        <div v-for="(item, index) in copartnerInfoList" :key="index">
-            <p class="small-title">{{ copartnerTitles[item.type] }}</p>
-            <div class="item-wrapper legal-form">
-                <el-form-item
-                    label="姓名："
-                    label-width="150px"
-                    :rules="item.type == 0 ? {required: true,message: '姓名不能为空',trigger: 'blur'} : {}"
-                    :prop="`copartnerInfoList[${index}].name`">
-                   {{item.name}}
-                </el-form-item>
-                <el-form-item
-                    label="联系方式"
-                    label-width="150px"
-                    :rules="item.type == 0 ? {required: true,message: '联系方式不能为空',trigger: 'blur'} : {required: false}"
-                    :prop="`copartnerInfoList[${index}].tel`">
-                    {{item.tel}}
-                </el-form-item>
-                <el-form-item
-                    label="性别："
-                    label-width="150px"
-                    :rules="item.type == 0 ? {required: true,message: '性别不能为空',trigger: 'change'} : {required: false}"
-                    :prop="`copartnerInfoList[${index}].sex`">
-                   {{item.sex==0?'男':'女'}}
-                </el-form-item>
-                <el-form-item
-                    label="婚姻："
-                    label-width="150px"
-                    :rules="item.type == 0 ? {required: true,message: '婚姻不能为空'} : {required: false}"
-                    :prop="`copartnerInfoList[${index}].marriage`">
-                    {{marriageOptions[item.marriage]}}
-                </el-form-item>
-                <el-form-item
-                    label="身份证号："
-                    label-width="150px"
-                    :rules="item.type == 0 ? {required: true,message: '身份证号不能为空',trigger: 'blur'} : {}"
-                    :prop="`copartnerInfoList[${index}].idNumber`">
-                 {{item.idNumber}}
-                </el-form-item>
-                <el-form-item label="学历：" label-width="150px">
-                    {{educationOptions[item.education]}}
-                </el-form-item>
-                <el-form-item label="持股比例：" label-width="150px" v-if="item.type != 2">
-                   {{item.shareholdingRatio}}
-                </el-form-item>
-                <el-form-item label="是否在外兼职：" label-width="150px">
-                   {{item.partTimeJob==0?'是':item.partTimeJob=1?'否':''}}
-                </el-form-item>
-                <el-form-item label="行业身份：" label-width="150px">
-                  {{item.industryStatus}}
-                </el-form-item>
-                <el-form-item label="个人经营能力：" label-width="150px" v-if="item.type == 0 || item.type == 5">
-                  {{item.managementAbility}}
-                </el-form-item>
-                <el-form-item label="行业口碑：" label-width="150px" v-if="item.type == 0">
-                {{item.industryReputation}}
-                </el-form-item>
-                <el-form-item label="社会资源：" label-width="150px">
-                   {{item.socialResources}}
-                </el-form-item>
+    <el-collapse-item name="2">
+        <template slot="title">
+            <p class="title-p">合伙人信息</p>
+        </template>
+        <template v-if="copartnerInfoList && copartnerInfoList.length > 0">
+            <div v-for="(item, index) in copartnerInfoList" :key="index">
+                <p class="small-title">{{ copartnerTitles[item.type] }}</p>
+                <div class="item-wrapper legal-form">
+                    <el-form-item label="姓名：" label-width="120px">
+                        <p>{{item.name}}</p>
+                    </el-form-item>
+                    <el-form-item label="联系方式" label-width="120px">
+                        <p>{{item.tel}}</p>
+                    </el-form-item>
+                    <el-form-item label="性别：" label-width="120px">
+                        <p>{{item.sex==0?'男':'女'}}</p>
+                    </el-form-item>
+                    <el-form-item label="婚姻：" label-width="120px">
+                        <p>{{item.marriage?marriageOptions[item.marriage].label:''}}</p>
+                    </el-form-item>
+                    <el-form-item label="身份证号：" label-width="120px">
+                        <p>{{item.idNumber}}</p>
+                    </el-form-item>
+                    <el-form-item label="学历：" label-width="120px">
+                        <p>{{item.education?educationOptions[item.education].label:''}}</p>
+                    </el-form-item>
+                    <el-form-item label="持股比例：" label-width="120px" v-if="item.type != 2">
+                        <p>{{item.shareholdingRatio}}</p>
+                    </el-form-item>
+                    <el-form-item label="是否在外兼职：" label-width="120px">
+                        <p>{{item.partTimeJob==0?'是':item.partTimeJob=1?'否':''}}</p>
+                    </el-form-item>
+                    <el-form-item label="行业身份：" label-width="120px">
+                        <p>{{item.industryStatus}}</p>
+                    </el-form-item>
+                    <el-form-item label="个人经营能力：" label-width="120px" v-if="item.type == 0 || item.type == 5">
+                        <p>{{item.managementAbility}}</p>
+                    </el-form-item>
+                    <el-form-item label="行业口碑：" label-width="120px" v-if="item.type == 0">
+                        <p>{{item.industryReputation}}</p>
+                    </el-form-item>
+                    <el-form-item label="社会资源：" label-width="120px">
+                        <p>{{item.socialResources}}</p>
+                    </el-form-item>
+                </div>
             </div>
-        </div>
-    </template>
-</el-collapse-item>
+        </template>
+    </el-collapse-item>
 </template>
 
 <script>
@@ -122,9 +102,14 @@ export default {
 }
 .legal-form .el-form-item {
     display: inline-block;
+    margin-bottom: 0;
+
+    P{
+        display: inline-block;
+        width: 200px;
+    }
 }
-.small-title{
+.small-title {
     margin-bottom: 20px;
 }
-
 </style>
