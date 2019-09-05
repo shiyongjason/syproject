1ca6c1dd9dd7ed1cb6762df901cb935ea8d6594a
lj-sj <990375133@qq.com>
2019-09-05 09:26:15 +0800

    change el-date-picker type

--- src/views/serviceManagement/customerManagement/components/addOrUpdata.vue
+++ src/views/serviceManagement/customerManagement/components/addOrUpdata.vue
@@ -52,7 +52,7 @@
                 <el-row>
                     <el-col :span="12" v-if="!isAdd">
                         <el-form-item prop="createTime" label="成为客户">
-                            <el-date-picker v-model="customerForm.createTime" :disabled="!isAdd" type="date" format="yyyy-MM-dd" placeholder="成为客户日期">
+                            <el-date-picker v-model="customerForm.createTime" :disabled="!isAdd" type="datetime" value-format='yyyy-MM-dd HH:mm:ss' placeholder="成为客户日期">
                             </el-date-picker>
                         </el-form-item>
                     </el-col>
