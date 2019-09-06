f2ce7613b0233285d494a23baac49b20d863ab06
bihaoze <923965895@qq.com>
2019-09-06 10:59:59 +0800

    config配置

--- src/views/bestonline/justice/components/legalInfo/companyBasic.vue
+++ src/views/bestonline/justice/components/legalInfo/companyBasic.vue
@@ -39,7 +39,7 @@
                 <i class="el-icon-remove-outline pointer" v-else @click="onRemovebranchAgencyList(item.id)"></i>
             </template>
             <el-form label-position="right" label-width="150px" class="legal-form">
-                <el-form-item label="分支机构" :rules="{required: true ,message: '分支机构不能为空',trigger: 'blur'}" :prop="`branchAgencyList[${index}].branch`">
+                <el-form-item label="分支机构" :prop="`branchAgencyList[${index}].branch`" :rules="rules.branch">
                     <el-input v-model="item.branch" placeholder="分支机构" maxlength="25"></el-input>
                 </el-form-item>
             </el-form>
@@ -76,6 +76,9 @@ export default {
             rules: {
                 relatedCompany: [
                     { required: true, message: '关联公司不能为空', trigger: 'blur' }
+                ],
+                branch: [
+                    { required: true, message: '分支机构不能为空', trigger: 'blur' }
                 ]
             }
         }
@@ -90,27 +93,29 @@ export default {
                 id: Date.now()
             }
         },
-        branchAgencyList () {
-            let branchAgencyList = this.justiceData.branchAgencyList
-            if (branchAgencyList.length === 0) {
-                const { ...tempObj } = this.defaultBranchAgency
-                branchAgencyList.push(tempObj)
-            }
-            return branchAgencyList
-        },
         defaultRelatedCompany () {
             return {
                 relatedCompany: '',
                 id: Date.now()
             }
-        },
-        relatedCompanyList () {
-            let relatedCompanyList = this.justiceData.relatedCompanyList
+        }
+    },
+    watch: {
+        'justiceData.relatedCompanyList' (val) {
+            let relatedCompanyList = val
             if (relatedCompanyList.length === 0) {
                 const tempObj = JSON.parse(JSON.stringify(this.defaultRelatedCompany))
                 relatedCompanyList.push(tempObj)
             }
-            return relatedCompanyList
+            this.justiceData.relatedCompanyList = relatedCompanyList
+        },
+        'justiceData.branchAgencyList' (val) {
+            let branchAgencyList = val
+            if (branchAgencyList.length === 0) {
+                const { ...tempObj } = this.defaultBranchAgency
+                branchAgencyList.push(tempObj)
+            }
+            justiceData.branchAgencyList = branchAgencyList
         }
     },
     methods: {
@@ -179,7 +184,7 @@ export default {
         height: 75px;
     }
 }
-/deep/.flex-wrap-row{
+/deep/.flex-wrap-row {
     .el-form-item__content {
         display: inline-block;
     }
