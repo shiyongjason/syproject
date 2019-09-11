6088c20c4cdb59f09026b37823fb4273f416e4a7
zhangjie <jie.zhang@hosjoy.com>
2019-09-11 11:04:18 +0800

    操作label的问题

--- src/views/bestonline/review.vue
+++ src/views/bestonline/review.vue
@@ -93,8 +93,7 @@ export default {
                 { label: '最终得分', prop: 'finalScore' },
                 { label: '标准分数', prop: 'standardScore' },
                 { label: '状态', prop: 'status' },
-                { label: '未操作人', prop: 'noOperator' },
-                { label: '操作', prop: 'action' }
+                { label: '未操作人', prop: 'noOperator' }
             ],
             tableData: [],
             pagination: {},
