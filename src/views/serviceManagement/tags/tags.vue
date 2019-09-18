88459625dd8ac306b33c5bbf8bf78beb91deba98
wangqi <wangqi@hosjoy.com>
2019-09-18 15:11:05 +0800

    标签管理

--- src/views/serviceManagement/tags/tags.vue
+++ src/views/serviceManagement/tags/tags.vue
@@ -4,7 +4,7 @@
             <div class="query-cont-col">
                 <div class="query-col-title">标签名：</div>
                 <div class="query-col-input">
-                    <el-input type="text" maxlength="50" v-model="queryParams" placeholder="请输入标签名">
+                    <el-input type="text" maxlength="20" v-model="queryParams.keywords" placeholder="请输入标签名">
                     </el-input>
                 </div>
             </div>
@@ -19,23 +19,27 @@
                 <el-button type="primary" class="ml20" @click="dialogVisible = true">
                     新建标签
                 </el-button>
-                <el-button type="primary" class="ml20" @click="deleteTags(multipleSelection.toString())">批量删除</el-button>
+                <!--<el-button type="primary" class="ml20" @click="deleteTags(multipleSelection.toString())">批量删除</el-button>-->
             </div>
         </div>
         <div class="page-body-cont">
-            <el-table :data="tableData" ref="multipleTable" border style="width: 100%" @selection-change="handleSelectionChange">
-                <el-table-column
-                    type="selection"
-                    width="55"></el-table-column>
-                <el-table-column prop="sourceName" label="标签名" align="center">
+            <el-table :data="tableData" ref="multipleTable" border style="width: 100%">
+                <!--@selection-change="handleSelectionChange"-->
+                <!--<el-table-column-->
+                    <!--type="selection"-->
+                    <!--width="55"></el-table-column>-->
+                <el-table-column prop="labelName" label="标签名" align="center">
                 </el-table-column>
-                <el-table-column prop="sourceName" label="客户使用人数" align="center">
+                <el-table-column prop="userNumbers" label="客户使用人数" align="center">
                 </el-table-column>
-                <el-table-column prop="sourceName" label="标签类型" align="center">
+                <el-table-column label="标签类型" align="center">
+                    <template slot-scope="scope">
+                        {{scope.row.labelType === 1 ? '手动标签' : '自动标签'}}
+                    </template>
                 </el-table-column>
                 <el-table-column prop="sourceName" label="操作" align="center">
                     <template slot-scope="scope">
-                        <el-button @click="deleteTags(scope.row)" class="orangeBtn">删除</el-button>
+                        <el-button @click="deleteTags(scope.row.id)" class="orangeBtn">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -57,7 +61,7 @@
                 <div class="query-cont-col">
                     <div class="query-col-title">标签名称：</div>
                     <div class="query-col-input">
-                        <el-input type="text" maxlength="50" v-model="addTags.name" placeholder="请输入标签名">
+                        <el-input type="text" maxlength="20" v-model="addTags.labelName" placeholder="请输入标签名">
                         </el-input>
                     </div>
                 </div>
@@ -66,7 +70,7 @@
                 <div class="query-cont-col">
                     <div class="query-col-title">标签类型：</div>
                     <div class="query-col-input">
-                        <el-radio v-model="addTags.type" label="1">手动标签</el-radio>
+                        <el-radio v-model="addTags.labelType" label="1">手动标签</el-radio>
                     </div>
                 </div>
             </div>
@@ -80,28 +84,37 @@
 
 <script>
 import { findTagsList, createTags, deleteTags } from './api/index'
+import { mapState } from 'vuex'
 export default {
     name: 'tags',
+    computed: {
+        ...mapState({
+            userInfo: state => state.userInfo
+        })
+    },
     data () {
         return {
-            queryParams: '',
+            queryParams: {
+                pageNumber: 1,
+                pageSize: 10,
+                keywords: ''
+            },
             tableData: [],
             paginationData: {
                 pageNumber: 1,
                 pageSize: 10,
                 totalElements: 100
             },
-            dialogVisible: true,
+            dialogVisible: false,
             addTags: {
-                name: '',
-                type: '1'
+                labelName: '',
+                labelType: '1'
             },
             multipleSelection: []
         }
     },
     mounted () {
         this.onQuery()
-        console.log(1)
     },
     methods: {
         async onQuery () {
@@ -114,21 +127,27 @@ export default {
             }
         },
         async createTags () {
-            await createTags(this.addTags)
+            const params = { ...this.addTags }
+            params.createBy = this.userInfo.employeeName
+            await createTags(params)
             this.dialogVisible = false
-            this.addTags.name = ''
+            this.addTags.labelName = ''
             this.onQuery()
         },
-        async deleteTags (params) {
+        async deleteTags (id) {
+            const params = {
+                id: id,
+                updateBy: this.userInfo.employeeName
+            }
             await deleteTags(params)
             this.onQuery()
         },
         handleSizeChange (val) {
-            this.paginationData.pageSize = val
+            this.queryParams.pageSize = val
             this.onQuery()
         },
         handleCurrentChange (val) {
-            this.paginationData.pageNumber = val
+            this.queryParams.pageNumber = val
             this.onQuery()
         },
         handleSelectionChange (val) {
@@ -136,7 +155,7 @@ export default {
         },
         cancel () {
             this.dialogVisible = false
-            this.addTags.name = ''
+            this.addTags.labelName = ''
         }
     }
 }
