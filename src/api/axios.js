2b43e73b0137c192924cc8d121d880be4687944b
hosjoy-sj <990375133@qq.com>
2019-09-18 18:22:59 +0800

    axios 取消重复请求

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,73 +1,85 @@
-/**
-* axios 全局配置
-*/
 import axios from 'axios'
-import { interfaceUrl } from './config'
-import { Message } from 'element-ui'
 import store from '@/store/index'
+import { Message } from 'element-ui'
+import { interfaceUrl } from './config'
 
-// 配置全局axios请求前缀
+const TIME_OUT = 6000 // 连接超时时间
+const token = sessionStorage.getItem('token')
+
+const configUrl = [{ method: 'get', url: 'api/login/bossLogin' }]
+/* const http = axios.create({
+    baseURL: `${interfaceUrl}`,
+    timeout: TIME_OUT
+}) */
 axios.defaults.baseURL = interfaceUrl
-const configUrl = [{
-    method: 'get',
-    url: 'api/login/bossLogin'
-}]
-// 配置request过滤器
-axios.interceptors.request.use(function (config) {
-    const token = sessionStorage.getItem('token')
-    if (token) {
-        config.headers['Authorization'] = 'Bearer ' + token
+axios.defaults.timeout = TIME_OUT
+const requestArr = []
+/** 声明一个数组用于存储每个请求的取消函数和标识(请求如果还在pending，同个请求就被取消) */
+const cancelRequst = (config) => {
+    for (const key in requestArr) {
+        if (requestArr[key].url === `${config.url}&${config.method}`) { // 当当前请求在数组中存在时执行函数体
+            requestArr[key].cancel('取消重复请求') // 执行取消操作
+            requestArr.splice(Number(key), 1) // 把这条记录从数组中移除
+        }
     }
-    return config
-}, function (error) {
-    return Promise.reject(error)
-})
+}
 
-// 配置response过滤器
-axios.interceptors.response.use(function (response) {
-    // TODO 处理 老boss用户不存在不提示错误直接进入首页
-    const config = response.config
-    const oldOss = configUrl.filter(item => item.method === config.method && config.url.indexOf(item.url) > -1)
-    if (oldOss.length > 0) {
+// 添加请求拦截器、取消请求
+const CancelToken = axios.CancelToken
+axios.interceptors.request.use(
+    (config) => {
+        token && (config.headers['Authorization'] = `Bearer ${token}`)
+        cancelRequst(config) // 在一个请求发送前执行一下取消操作
+        config.cancelToken = new CancelToken(cancelMethod => {
+            requestArr.push({ url: `${config.url}&${config.method}`, cancel: cancelMethod })
+        })
+        // store.commit('LOAD_STATE', true)
+        return config
+    },
+    (error) => {
+        Promise.reject(error)
+    }
+)
+// 添加响应拦截器
+axios.interceptors.response.use(
+    (response) => {
+        cancelRequst(response.config)// 请求响应后，把已经完成的请求从requestArr中移除
+        // TODO 处理 老boss用户不存在不提示错误直接进入首页
+        const config = response.config
+        const oldOss = configUrl.filter(item => item.method === config.method && config.url.indexOf(item.url) > -1)
+        if (oldOss.length > 0) {
+            store.commit('LOAD_STATE', false)
+            return response
+        }
+        if (response.data.code && response.data.code != 200) {
+            Message({
+                message: response.data.msg || response.data.message,
+                type: 'error'
+            })
+            store.commit('LOAD_STATE', false)
+            return Promise.reject(response)
+        }
         store.commit('LOAD_STATE', false)
         return response
-    }
-    // TODO status==200 异常处理 200 || '200'
-    if (response.data.code && response.data.code != 200) {
+    },
+    (error) => {
+        console.log(error)
+        if (error.message && error.message == '取消重复请求') {
+            return Promise.reject(error)
+        }
+        let errorMessage = ''
+        if (error.response && error.response.status === 400) {
+            errorMessage = error.response.data.message
+        } else {
+            errorMessage = '服务器响应错误：' + error
+        }
+        store.commit('LOAD_STATE', false)
         Message({
-            message: response.data.msg || response.data.message,
+            message: errorMessage,
             type: 'error'
         })
-        return Promise.reject(response)
-    }
-    store.commit('LOAD_STATE', false)
-    return response
-}, function (error) {
-    // TODO: 后面还是按照后台返回401解决 error.response.status === 401
-    if (error.config.url.indexOf('/uaa/api/auth/details') > -1 && error.config.timeout === 0) {
-        Message({
-            message: '权限无效，已为你重定向到登录页',
-            type: 'error'
-        })
-        setTimeout(function () {
-            location.href = '/login'
-        }, 1200)
         return Promise.reject(error)
     }
-    // if (error.request.status === 0 && error.request.readyState === 4 && error.request.timeout === 0) {
-    // Message({ message: '网络异常，请检查网络链接', type: 'error' })
-    // return Promise.reject(error)
-    // }
-    store.commit('LOAD_STATE', false)
-    const data = error.response.data
-    let message = '服务器响应错误：' + error
-    if (error.response.status === 400 && data.message !== '') {
-        message = data.message
-    }
-    // TODO: 异常统一处理
-    Message({
-        message,
-        type: 'error'
-    })
-    return Promise.reject(error)
-})
+)
+
+// export default http
