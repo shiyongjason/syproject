7389955ce9700752cdf1a7985dda1f8e5ff8d67e
wangqi <wangqi@hosjoy.com>
2019-09-23 14:21:59 +0800

    订单导入

--- src/views/serviceManagement/orderCenter/components/searchForm.vue
+++ src/views/serviceManagement/orderCenter/components/searchForm.vue
@@ -2,12 +2,11 @@
     <div class="search page-body-cont query-cont">
         <div class="search-head">
             <div class="query-cont-row">
-                <el-form :model="searchForm" ref="form">
+                <el-form :model="value" ref="form">
                     <div class="query-cont-col">
                         <div class="query-col-title">订单搜索：</div>
                         <div class="query-col-input">
-                            <el-select v-model="searchForm.searchKey" clearable>
-                                <el-option label="全部" value=""></el-option>
+                            <el-select v-model="value.searchKey">
                                 <el-option label="订单号" value="orderNo"></el-option>
                                 <el-option label="外部订单号" value="channelOrderNo"></el-option>
                                 <el-option label="姓名" value="userName"></el-option>
@@ -15,23 +14,23 @@
                             </el-select>
                         </div>
                         <div class="m-l">
-                            <el-input v-model="searchForm.searchValue" style="width:250px" clearable :placeholder="getPlaceholderText" :maxlength="inputLength" />
+                            <el-input v-model="value.searchValue" style="width:250px" clearable :placeholder="getPlaceholderText" :maxlength="inputLength" />
                         </div>
                     </div>
                     <div class="query-cont-col">
                         <div class="query-col-title">下单时间：</div>
                         <div class="query-col-input">
-                            <el-date-picker v-model="searchForm.startTime" value-format='yyyy-MM-dd HH:mm:ss' type="datetime" placeholder="开始日期" :picker-options="pickerOptionsStart">
+                            <el-date-picker v-model="value.startTime" value-format='yyyy-MM-dd HH:mm:ss' type="datetime" placeholder="开始日期" :picker-options="pickerOptionsStart">
                             </el-date-picker>
                             <span class="ml10 mr10"> --</span>
-                            <el-date-picker v-model="searchForm.endTime" value-format='yyyy-MM-dd HH:mm:ss' type="datetime" placeholder="结束日期" :picker-options="pickerOptionsEnd">
+                            <el-date-picker v-model="value.endTime" value-format='yyyy-MM-dd HH:mm:ss' type="datetime" placeholder="结束日期" :picker-options="pickerOptionsEnd">
                             </el-date-picker>
                         </div>
                     </div>
                     <div class="query-cont-col">
                         <div class="query-col-title">渠道名称：</div>
                         <div class="query-col-input">
-                            <el-select v-model="searchForm.source" clearable>
+                            <el-select v-model="value.source" clearable>
                                 <el-option label="全部" value=""></el-option>
                                 <el-option label="有赞商城" value="1"></el-option>
                                 <el-option label="孩子王" value="2"></el-option>
@@ -66,7 +65,7 @@
 </template>
 
 <script>
-    import {interfaceUrl} from '@/api/config'
+import { interfaceUrl } from '@/api/config'
 export default {
     name: 'searchForm',
     props: ['value'],
@@ -83,29 +82,21 @@ export default {
     },
     computed: {
         inputLength () {
-            if (this.searchForm.searchKey === 'mobile') {
+            if (this.value.searchKey === 'mobile') {
                 return '11'
             }
             return ''
         },
         getPlaceholderText () {
-            if (this.searchForm.searchKey) {
-                return `请输入${this.type[this.searchForm.searchKey]}`
+            if (this.value.searchKey) {
+                return `请输入${this.type[this.value.searchKey]}`
             }
             return '请输入'
-        },
-        searchForm: {
-            get () {
-                return this.value
-            },
-            set (val) {
-                this.$emit('input', val)
-            }
         },
         pickerOptionsStart () {
             return {
                 disabledDate: time => {
-                    let beginDateVal = this.searchForm.endTime
+                    let beginDateVal = this.value.endTime
                     if (beginDateVal) {
                         return (
                             time.getTime() > new Date(beginDateVal).getTime()
@@ -117,7 +108,7 @@ export default {
         pickerOptionsEnd () {
             return {
                 disabledDate: time => {
-                    let beginDateVal = this.searchForm.startTime
+                    let beginDateVal = this.value.startTime
                     if (beginDateVal) {
                         return (
                             time.getTime() < new Date(beginDateVal).getTime()
@@ -129,28 +120,25 @@ export default {
     },
     methods: {
         onSearch () {
-            if (this.searchForm.searchKey) {
-                this.$set(this.searchForm, this.searchForm.searchKey, this.searchForm.searchValue)
-            } else {
-                if (this.searchForm.searchValue) this.searchForm.searchValue = null
-            }
-            this.$emit('search', this.searchForm)
+            this.$emit('search')
         },
         isSuccess (response) {
-            console.log(response)
-            this.$emit('search', this.searchForm)
-            // if (response.code !== 200) {
-            //     this.$message({
-            //         message: '批量导入失败，' + response.message,
-            //         type: 'error'
-            //     })
-            // } else {
-            //     this.$message({
-            //         message: '批量导入成功！',
-            //         type: 'success'
-            //     })
-            //
-            // }
+            if (response) {
+                this.$alert(`<span style="color: red">警告</span>：</br>${response.split(',').join('</br>')}`, '导入状态', {
+                    confirmButtonText: '确定',
+                    dangerouslyUseHTMLString: true,
+                    showClose: false
+                }).then(async () => {
+                    this.$emit('search', this.value)
+                })
+            } else {
+                this.$alert('导入成功', '导入状态', {
+                    confirmButtonText: '确定',
+                    showClose: false
+                }).then(async () => {
+                    this.$emit('search', this.value)
+                })
+            }
         }
     }
 }
--- src/views/serviceManagement/orderCenter/order.vue
+++ src/views/serviceManagement/orderCenter/order.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="order page-body">
-        <searchForm @search='onSearch' v-model="searchForm" />
+        <searchForm @search='onSearch' v-model="searchForm"/>
         <order-table :tableData='list' @search-event="getData"/>
         <div class="pages">
             <el-pagination background layout="total, sizes, prev, pager, next, jumper" :current-page="queryParams.pageNumber" :page-sizes="page.sizes" :page-size="queryParams.pageSize" :total="page.total" @size-change="handleSizeChange" @current-change="handleCurrentChange"></el-pagination>
@@ -25,8 +25,7 @@ export default {
         return {
             list: [],
             searchForm: {
-                searchKey: '',
-                source: ''
+                searchKey: 'orderNo'
             }
         }
     },
@@ -36,7 +35,11 @@ export default {
             this.getData()
         },
         async getData (param) {
-            let params = { ...this.searchForm, ...this.queryParams }
+            const temp = { ...this.searchForm }
+            temp[temp.searchKey] = temp.searchValue
+            delete temp.searchKey
+            delete temp.searchValue
+            let params = { ...temp, ...this.queryParams }
             if (param && param.status && param.status !== '0') params.status = param.status
             const { data } = await findOrderList(params)
             this.list = data.records
