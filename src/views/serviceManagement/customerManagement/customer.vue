f2550372de8f536021888e71d961adb4afa70ec2
wangqi <wangqi@hosjoy.com>
2019-09-26 10:00:10 +0800

    文案修改

--- src/views/serviceManagement/customerManagement/customer.vue
+++ src/views/serviceManagement/customerManagement/customer.vue
@@ -33,7 +33,7 @@ export default {
             },
             editInfo: {},
             channelType: [
-                { value: '', label: '全部' }, { value: 0, label: '总部' }, { value: 1, label: '有赞商城' }, { value: 2, label: '孩子王成长家' }, { value: 3, label: '考拉买菜' }
+                { value: '', label: '全部' }, { value: 0, label: '总部' }, { value: 1, label: '有赞商城' }, { value: 2, label: '孩子王' }, { value: 3, label: '考拉买菜' }
             ],
             role: [
                 { value: '', label: '全部' }, { value: 0, label: '客户' }, { value: 1, label: '线下管家' }, { value: 2, label: '线上管家' }
