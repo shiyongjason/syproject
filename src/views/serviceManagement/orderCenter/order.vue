98920159f6fdfa408739353834494282d63f1c90
wangqi <wangqi@hosjoy.com>
2019-09-27 20:42:03 +0800

    bug修复

--- src/views/serviceManagement/orderCenter/order.vue
+++ src/views/serviceManagement/orderCenter/order.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="order page-body">
         <searchForm @search='onSearch' @onClear="onClear" v-model="searchForm"/>
-        <order-table :tableData='list' @search-event="getData"/>
+        <order-table :tableData='list' @search-event="onChangeStauts"/>
         <div class="pages">
             <el-pagination background layout="total, sizes, prev, pager, next, jumper" :current-page="queryParams.pageNumber" :page-sizes="page.sizes" :page-size="queryParams.pageSize" :total="page.total" @size-change="handleSizeChange" @current-change="handleCurrentChange"></el-pagination>
         </div>
@@ -31,6 +31,10 @@ export default {
         }
     },
     methods: {
+        onChangeStauts (value) {
+            this.queryParams.status = value.status
+            this.getData()
+        },
         onClear () {
             this.searchForm = {
                 searchKey: 'orderNo',
@@ -42,13 +46,12 @@ export default {
             this.queryParams.pageNumber = 1
             this.getData()
         },
-        async getData (param) {
+        async getData () {
             const temp = { ...this.searchForm }
             temp[temp.searchKey] = temp.searchValue
             delete temp.searchKey
             delete temp.searchValue
             let params = { ...temp, ...this.queryParams }
-            if (param && param.status && param.status !== '0') params.status = param.status
             const { data } = await findOrderList(params)
             this.list = data.records
             this.page.total = data.total
@@ -57,7 +60,6 @@ export default {
     mounted () {
         let defaultMobile = this.$route.query.mobile
         if (defaultMobile) {
-            console.log(1)
             this.searchForm.searchKey = 'mobile'
             this.searchForm.searchValue = defaultMobile
         }
