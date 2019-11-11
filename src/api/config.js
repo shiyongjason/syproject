89b01f4ead316372a3dd724e5f27fa1f0360884a
hosjoy-sj <990375133@qq.com>
2019-11-11 14:29:25 +0800

    add

--- src/api/config.js
+++ src/api/config.js
@@ -13,23 +13,20 @@ var env = process.env.NODE_ENV === 'development' ? 'development' : (process.env.
 // var env = process.env.NODE_ENV === 'development' ? 'development' : (process.env.VUE_APP_TITLE === 'dev' ? 'dev' : process.env.VUE_APP_TITLE === 'test' ? 'test' : 'production')
 switch (env) {
     case 'development':
-        interfaceUrl = 'http://192.168.20.248:30000/'
+        /* interfaceUrl = 'http://192.168.20.248:30000/'
         ossUrl = 'http://192.168.20.248:8089/'
         iframeUrl = 'http://devcrm.hosjoy.com/'
         jinyun = 'https://jinyun.hosjoy.cn/wfsf/'
         ccpBaseUrl = 'http://192.168.20.248:8021/'
         B2bUrl = 'http://192.168.20.248:40000/'
-        fileUploadUrl = 'http://192.168.20.248:30000/'
-        /* interfaceUrl = 'https://testhbp.hosjoy.com:4832/'
+        fileUploadUrl = 'http://192.168.20.248:30000/' */
         ossUrl = 'https://testroute.hosjoy.com:4832/'
-        uploadUrl = 'https://testhbp.hosjoy.com:4832/tms'
-        fileUploadUrl = 'http://192.168.20.248:30000/'
-        /* interfaceUrl = 'https://testhbp.hosjoy.com:4832/'
+        interfaceUrl = 'https://testhbp.hosjoy.com:4832/'
         ossUrl = 'https://testroute.hosjoy.com:4832/'
         iframeUrl = 'https://test.hosjoy.com:4832/'
         jinyun = 'https://jinyun.hosjoy.cn/wfsf/'
         B2bUrl = 'https://testb2b-gateway.hosjoy.com:4832/'
-        fileUploadUrl = 'https://testhbp.hosjoy.com:4832/' */
+        fileUploadUrl = 'https://testhbp.hosjoy.com:4832/'
         break
     case 'dev':
         interfaceUrl = 'http://192.168.20.248:30000/'
