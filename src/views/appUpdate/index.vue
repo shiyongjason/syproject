4e9131cb5c6194874493c57235d674b83a28acf6
sunhuiyong <shy520134@163.com>
2019-11-25 14:22:35 +0800

    1

--- src/views/appUpdate/index.vue
+++ src/views/appUpdate/index.vue
@@ -4,17 +4,32 @@
             <div class="query-cont-col">
                 <div class="query-col-title">产品：</div>
                 <div class="query-col-input">
-                    <el-input type="text" maxlength="20" v-model="queryParams.customerName" placeholder="请输入客户名称">
-                    </el-input>
+                    <el-select v-model="queryParams.produet" clearable>
+                        <el-option v-for="(item,index) in produetType" :key="index" :label="item.label" :value="item.value">
+                        </el-option>
+                    </el-select>
+                </div>
+            </div>
+            <div class="query-cont-col">
+                <div class="query-col-title">平台：</div>
+                <div class="query-col-input">
+                    <el-select v-model="queryParams.statusId" clearable>
+                        <el-option v-for="(item,index) in statusIdType" :key="index" :label="item.label" :value="item.value">
+                        </el-option>
+                    </el-select>
                 </div>
             </div>
             <div class="query-cont-col">
                 <div class="query-col-title">创建时间：</div>
                 <div class="query-col-input">
-                    <el-date-picker v-model="queryParams.createTime" type="date" value-format='yyyy-MM-dd' placeholder="请选择时间" :picker-options="pickerOptionsStart">
+                    <el-date-picker v-model="queryParams.createTime" type="date" value-format='yyyy-MM-dd' placeholder="开始日期" :picker-options="pickerOptionsStart">
+                    </el-date-picker>
+                    <span class="ml10 mr10">-</span>
+                    <el-date-picker v-model="queryParams.endTime" type="date" value-format='yyyy-MM-dd' placeholder="请选择时间" :picker-options="pickerOptionsEnd">
                     </el-date-picker>
                 </div>
             </div>
+
             <div class="query-cont-col">
                 <div class="query-col-title">
                     <el-button type="primary" class="ml20" @click="onSearch">搜索</el-button>
@@ -26,26 +41,13 @@
         </div>
         <div class="page-body-cont query-cont">
             <!-- 按钮权限 -->
-            <div class="query-cont-col" v-if="hosAuthCheck(reCheckAuth)">
-                <el-button type="primary" class="ml20" @click="toDo(1)">同意</el-button>
-                <el-button type="primary" class="ml20" @click="toDo(2)">拒绝</el-button>
-            </div>
-            <div class="query-cont-col" v-if="hosAuthCheck(importAuth)">
-                <el-upload class="upload-demo" :show-file-list="false" :action="interfaceUrl + 'backend/amount/rate/import'" :on-success="isSuccess" :on-error="isError" auto-upload>
-                    <el-button type="primary" class="ml20">
-                        导入
-                    </el-button>
-                </el-upload>
+            <div class="query-cont-col">
+                <el-button type="primary" class="ml20" @click="onNewVersion">新建版本</el-button>
             </div>
         </div>
         <div class="page-body-cont">
-            <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-                <el-tab-pane label="待生效" name="001"></el-tab-pane>
-                <el-tab-pane label="生效" name="000"></el-tab-pane>
-                <el-tab-pane label="失效" name="002"></el-tab-pane>
-            </el-tabs>
             <!-- 表格使用老毕的组件 -->
-            <basicTable :tableLabel="tableLabel" :tableData="tableData" :pagination="pagination" @onCurrentChange='onCurrentChange' @onSizeChange='onSizeChange' :isMultiple='isMultiple && isTab' @update:multiSelection='multiSelection'>
+            <basicTable :tableLabel="tableLabel" :tableData="tableData" :pagination="pagination" @onCurrentChange='onCurrentChange' @onSizeChange='onSizeChange'>
             </basicTable>
         </div>
         <el-dialog title="提示" :visible.sync="resultDialogVisible" :close-on-click-modal='false' width="30%" center>
@@ -71,7 +73,7 @@ export default {
         pickerOptionsStart () {
             return {
                 disabledDate: time => {
-                    let beginDateVal = this.queryParams.createTimeEnd
+                    let beginDateVal = this.queryParams.endTime
                     if (beginDateVal) {
                         return (
                             time.getTime() > new Date(beginDateVal).getTime()
@@ -83,38 +85,25 @@ export default {
         pickerOptionsEnd () {
             return {
                 disabledDate: time => {
-                    let beginDateVal = this.queryParams.createTimeStart
+                    let beginDateVal = this.queryParams.createTime
                     if (beginDateVal) {
                         return (
-                            time.getTime() < new Date(beginDateVal).getTime()
+                            time.getTime() < new Date(beginDateVal).getTime() - 3600 * 1000 * 24
                         )
                     }
                 }
-            }
-        },
-        isMultiple () {
-            return this.hosAuthCheck(this.reCheckAuth)
-        }
-    },
-    watch: {
-        activeName (val) {
-            if (val == '001') {
-                this.isTab = true
-            } else {
-                this.isTab = false
             }
         }
     },
     data () {
         return {
-            activeName: '001',
             importAuth: JINYUN_AMOUNT_IMPORT_IMPORT,
             reCheckAuth: JINYUN_AMOUNT_IMPORT_RE_CHECK,
             interfaceUrl: interfaceUrl,
             queryParams: {
                 pageNumber: 1,
                 pageSize: 10,
-                statusId: '001'
+                statusId: ''
             },
             searchParams: {},
             tableData: [],
@@ -129,54 +118,36 @@ export default {
             },
             multipleSelection: [],
             tableLabel: [
-                { label: '客户名称', prop: 'customerName' },
-                { label: 'MIS编码', prop: 'misCode' },
-                { label: '年度最高额（元）', prop: 'yearlyQuota', formatters: 'money' },
-                { label: '月度滚动额（元）', prop: 'monthlyQuota', formatters: 'money' },
-                { label: '应收账款扣减额（元）', prop: 'accountReceivableQuota', formatters: 'money' },
-                { label: '实时用信额（元）', prop: 'dailyQuota', formatters: 'money' },
-                { label: '本月利率(年化）', prop: 'dailyInterestRate' },
-                { label: '创建日期', prop: 'importDate' },
-                { label: '当前状态', prop: 'statusId' }
+                { label: '产品', prop: 'customerName' },
+                { label: '平台', prop: 'customerName' },
+                { label: '版本号', prop: 'customerName' },
+                { label: '版本状态', prop: 'customerName' },
+                { label: '强制更新', prop: 'customerName' },
+                { label: '更新描述', prop: 'customerName' },
+                { label: '下载地址', prop: 'customerName' },
+                { label: '创建者', prop: 'customerName' },
+                { label: '更新时间', prop: 'customerName' },
+                { label: '操作', prop: 'customerName' }
             ],
             statusIdType: [
-                { value: '', label: '请选择' },
-                { value: '001', label: '待生效' },
-                { value: '000', label: '生效' },
-                { value: '002', label: '失效' }
+                { value: '', label: '全部' },
+                { value: 'ios', label: '苹果' },
+                { value: 'android', label: '安卓' }
+            ],
+            produetType: [
+                { value: '', label: '全部' },
+                { value: 'dfx', label: '单分享' },
+                { value: 'iot', label: 'IOT' }
             ],
             resultDialogVisible: false,
-            content: '',
+            content: '新建版本',
             status: '',
-            // 控制权限
-            isTab: true,
-            multiSelect: []
         }
     },
     mounted () {
         this.onSearch()
     },
     methods: {
-        handleClick () {
-            this.queryParams.statusId = this.activeName
-            this.onSearch()
-        },
-        multiSelection (val) {
-            this.multiSelect = val
-        },
-        isSuccess (response) {
-            this.$message({
-                message: '批量导入成功！',
-                type: 'success'
-            })
-            this.onSearch()
-        },
-        isError (response) {
-            this.$message({
-                message: '批量导入失败，' + JSON.parse(response.message).message,
-                type: 'error'
-            })
-        },
         async onQuery () {
             // const { data } = await getRateList(this.queryParams)
             // this.tableData = data.records
@@ -204,19 +175,7 @@ export default {
             this.$set(this.queryParams, 'createTime', '')
             this.onSearch()
         },
-        async createTags () { },
-        toDo (i) {
-            if (this.multiSelect.length == 0) {
-                this.$alert('请勾选需要审批的条目！', '勾选提示', {
-                    confirmButtonText: '确定'
-                })
-                return
-            }
-            if (i == 2) {
-                this.content = '是否确认拒绝本次导入操作？'
-            } else {
-                this.content = '是否确认同意本次导入操作？'
-            }
+        onNewVersion (i) {
             this.status = i
             this.resultDialogVisible = true
         },
