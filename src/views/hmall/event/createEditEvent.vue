349f9bdfaed5f868d7b30af423b74ec51e025f70
hosjoy-sj <990375133@qq.com>
2019-11-28 10:00:29 +0800

    add

---
+++ src/views/hmall/event/createEditEvent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      createEditEvent
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'createEditEvent',
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+}
+</script>
+
+<style scoped></style>
