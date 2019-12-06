ac0bc58e3db84f90f64d4c32967e8bea087c12a3
wangqi <wangqi@hosjoy.com>
2019-12-06 10:13:59 +0800

    2.1.1功能开发

--- src/views/layout/Default.vue
+++ src/views/layout/Default.vue
@@ -217,13 +217,14 @@ export default {
         position: absolute;
         z-index: 2000;
         background-color: rgba(255, 255, 255, 0.9);
-        margin: 0;
         top: 0;
         right: 0;
         bottom: 0;
         left: 0;
         -webkit-transition: opacity 0.3s;
         transition: opacity 0.3s;
+        height: 100%;
+        width: 100%;
     }
 
     .edit-password {
