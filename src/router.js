92035fdccdcb098825df3752295d9d856f5a2725
wangqi <wangqi@hosjoy.com>
2019-12-06 14:46:34 +0800

    2.1.1功能开发

--- src/router.js
+++ src/router.js
@@ -861,8 +861,7 @@ const router = new Router({
             name: '403',
             component: () => import('./views/error/403'),
             hidden: true
-        },
-        ...routerMapping
+        }
     ]
 })
 function makeIndex (data, next, query) {
