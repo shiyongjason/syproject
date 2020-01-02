23b99b1fb0ae091022c2585584a75dfee3442dbe
lijun <jun.li@hosjoy.com>
2020-01-02 11:08:47 +0800

    HAM-7261 现场尽调-附件上传及删除问题 - 前端(等待联调)

--- src/views/bestonline/components/HAccessory.vue
+++ src/views/bestonline/components/HAccessory.vue
@@ -9,7 +9,12 @@
                 <p class="small-title">已上传附件</p>
                 <div v-if="tableList.length === 0" class="noannex">暂无附件</div>
                 <div v-else class="upload" v-for="(item,index) in tableList" :key="index">
-                    <span>{{item.fileName}}</span> <span>{{item.createUser}} {{item.createTime}}</span> <span> <a :href="item.fileUrl" target="_blank">下载</a></span>
+                    <span>{{item.fileName}}</span>
+                    <span>{{item.createUser}} {{item.createTime}}</span>
+                    <span>
+                        <span class="upload-delete">删除</span>
+                        <a :href="item.fileUrl" target="_blank">下载</a>
+                    </span>
                 </div>
                 <p class="small-title " v-if="roleType">附件上传</p>
                 <div class="upload" v-if="roleType">
@@ -18,6 +23,13 @@
                         <div slot="tip" class="el-upload__tip">附件格式除视频类的、录音类的暂时不需支持外，其他附件格式都支持。常见的一些附件格式：jpg,jpeg,png,pdf,word,xsl,xlsx,ppt,zip,rar,必须支持,附件每个大小限制100M以内</div>
                     </el-upload>
                 </div>
+                <!-- 开发时删除权限控制,结束开发时请删除 -->
+                <!-- <div class="upload">
+                    <el-upload class="upload-demo" v-bind="uploadInfo" :multiple="true" :on-success="handleSuccess" :before-remove="beforeRemove" :on-exceed="handleExceed" :file-list="fileList" :before-upload="handleUpload">
+                        <el-button size="small" type="primary">点击上传</el-button>
+                        <div slot="tip" class="el-upload__tip">附件格式除视频类的、录音类的暂时不需支持外，其他附件格式都支持。常见的一些附件格式：jpg,jpeg,png,pdf,word,xsl,xlsx,ppt,zip,rar,必须支持,附件每个大小限制100M以内</div>
+                    </el-upload>
+                </div> -->
                 <!--end-->
             </el-collapse-item>
         </el-collapse>
@@ -26,6 +38,12 @@
                 <el-button type="primary" @click="onSvaeattach" v-if="hosAuthCheck(commitAuthCode)">提交</el-button>
             </el-col>
         </div>
+        <!-- 开发时删除权限控制,结束开发时请删除 -->
+        <!-- <div class="flex-wrap-row">
+            <el-col :span="2" :offset="8">
+                <el-button type="primary" @click="onSvaeattach">提交</el-button>
+            </el-col>
+        </div> -->
     </div>
 </template>
 <script>
@@ -138,6 +156,7 @@ export default {
                     document.getElementsByClassName('el-icon-close')[i].dispatchEvent(e) // 这里的clickME可以换成你想触发行为的DOM结点
                 }
                 this.arrList = []
+                this.fileList = []
                 this.type = 0
                 this.$message({
                     showClose: true,
@@ -188,6 +207,10 @@ export default {
         &:last-child {
             color: #c31313;
         }
+    }
+    .upload-delete {
+        margin-right: 20px;
+        color: #F8B35A;
     }
 }
 .noannex {
