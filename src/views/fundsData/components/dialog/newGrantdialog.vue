590b09ec17beb6ac8913b051e10aa5f734c3f8c2
shiyong <422703012@qq.com>
2020-02-13 15:33:32 +0800

    HAM-7627 流贷 分受信弹窗

--- src/views/fundsData/components/dialog/newGrantdialog.vue
+++ src/views/fundsData/components/dialog/newGrantdialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- 新增流贷弹窗 -->
+    <!-- 新增分授信弹窗 -->
     <div class="flowbody page-body">
         <div class="page-body-cont query-cont">
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="130px" class="demo-ruleForm">
@@ -7,16 +7,14 @@
                 <div class="query-cont-row">
                     <div class="query-cont-col">
                         <el-form-item label="台账编号：" prop="name">
-                            <el-input v-model.trim="ruleForm.name"></el-input>
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入台账编号"></el-input>
                         </el-form-item>
                     </div>
                     <div class="query-cont-col">
                         <el-form-item label="金云系统编号：" prop="name">
-                            <el-input v-model.trim="ruleForm.name"></el-input>
+                            <el-input v-model.trim="ruleForm.name" placeholder="如有请输入，无请忽略"></el-input>
                         </el-form-item>
                     </div>
-                </div>
-                <div class="query-cont-row">
                     <div class="query-cont-col">
                         <el-form-item label="借款单位：" prop="name">
                             <el-input v-model.trim="ruleForm.name"></el-input>
@@ -36,6 +34,11 @@
                 <div class="dialogtitle">借款信息：</div>
                 <div class="query-cont-row">
                     <div class="query-cont-col">
+                        <el-form-item label="开票金额：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入开票金额"></el-input>
+                        </el-form-item>
+                    </div>
+                    <div class="query-cont-col">
                         <el-form-item label="供货商名称：" prop="name">
                             <el-input v-model.trim="ruleForm.name" placeholder="请输入供货商名"></el-input>
                         </el-form-item>
@@ -51,15 +54,20 @@
                         </el-form-item>
                     </div>
                     <div class="query-cont-col">
-                        <el-form-item label="放款日期：" prop="name">
-                            <el-input v-model.trim="ruleForm.name" placeholder="请输入年利率"><template slot="append">%</template></el-input>
-                        </el-form-item>
-                    </div>
-                    <div class="query-cont-col">
                         <el-form-item label="借款期限： " prop="name">
                             <el-radio v-model.trim="radio" label="月"></el-radio>
                             <el-radio v-model.trim="radio" label="天"></el-radio>
                             <el-input v-model.trim="ruleForm.name" placeholder="请输入借款期限"><template slot="append">{{radio?'月':'天'}}</template></el-input>
+                        </el-form-item>
+                    </div>
+                    <div class="query-cont-col">
+                        <el-form-item label="开票日期：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入年利率"><template slot="append">%</template></el-input>
+                        </el-form-item>
+                    </div>
+                    <div class="query-cont-col">
+                        <el-form-item label="借款日期：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入年利率"><template slot="append">%</template></el-input>
                         </el-form-item>
                     </div>
                     <div class="query-cont-col">
@@ -71,35 +79,68 @@
                 <div class="dialogtitle">还款信息：</div>
                 <div class="query-cont-col">
                     <el-form-item label="还款方式：" prop="name">
-                        一次性还款
+                        <el-radio v-model.trim="radio" label="1">334</el-radio>
+                        <el-radio v-model.trim="radio" label="0">一次性还款</el-radio>
                     </el-form-item>
                 </div>
-                <h3>逾期</h3>
+                <!-- <h3>逾期</h3>
+                <div class="query-cont-row">
+                    <el-form-item label="阶梯式计息：" prop="name">
+                        <el-radio v-model.trim="radio" label="1">否</el-radio>
+                        <el-radio v-model.trim="radio" label="1">是</el-radio>
+                    </el-form-item>
+                </div> -->
+                <div class="smalltitle">第一次还款：</div>
+                <div class="query-cont-row">
+                    <div class="query-cont-col">
+                        <el-form-item label="约定还款日期：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请选择约定还款日期"></el-input>
+                        </el-form-item>
+                    </div>
+                     <div class="query-cont-col">
+                        <el-form-item label="还款金额：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入还款金额"><template slot="append">元</template></el-input>
+                        </el-form-item>
+                    </div>
+                </div>
+               <div class="smalltitle">第二次还款：</div>
+                <div class="query-cont-row">
+                    <div class="query-cont-col">
+                        <el-form-item label="约定还款日期：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请选择约定还款日期"></el-input>
+                        </el-form-item>
+                    </div>
+                     <div class="query-cont-col">
+                        <el-form-item label="还款金额：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入还款金额"><template slot="append">元</template></el-input>
+                        </el-form-item>
+                    </div>
+                </div>
+                <div class="smalltitle">第三次还款：</div>
+                <div class="query-cont-row">
+                    <div class="query-cont-col">
+                        <el-form-item label="约定还款日期：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请选择约定还款日期"></el-input>
+                        </el-form-item>
+                    </div>
+                     <div class="query-cont-col">
+                        <el-form-item label="还款金额：" prop="name">
+                            <el-input v-model.trim="ruleForm.name" placeholder="请输入还款金额"><template slot="append">元</template></el-input>
+                        </el-form-item>
+                    </div>
+                </div>
+                <h3>逾期信息</h3>
                 <div class="query-cont-row">
                     <el-form-item label="阶梯式计息：" prop="name">
                         <el-radio v-model.trim="radio" label="1">否</el-radio>
                         <el-radio v-model.trim="radio" label="1">是</el-radio>
                     </el-form-item>
                 </div>
-                <div class="smalltitle">逾期第一阶段利息：</div>
-                <div class="query-cont-row">
-                    <div class="query-cont-col">
-                        <el-form-item label="第一阶段时长：" prop="name">
-                            <el-input v-model.trim="ruleForm.name" placeholder="请输入逾期时长"><template slot="append">月</template></el-input>
-                        </el-form-item>
-                    </div>
-                    <div class="query-cont-col">
-                        <el-form-item label="该阶段逾期利率：" prop="name">
+                <div class="query-cont-col">
+                        <el-form-item label="逾期利率：" prop="name">
                             <el-input v-model.trim="ruleForm.name" placeholder="请输入逾期利息"><template slot="append">%</template></el-input>
                         </el-form-item>
                     </div>
-                     <div class="query-cont-col">
-                          <el-button type="primary" size="small"  @click="onSubmit" icon="el-icon-minus">删除</el-button>
-                     </div>
-                </div>
-                <div>
-                    <el-button type="primary" size="small"  @click="onSubmit" icon="el-icon-plus">下阶段利息</el-button>
-                </div>
                 <div class="dialogtitle">档案信息：</div>
                 <div class="query-cont-row">
                     <div class="query-cont-col">
