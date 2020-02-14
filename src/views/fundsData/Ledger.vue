83b67cb23ff5d64842d44d67a41cf38accb17ba0
sunhuiyong <shy520134@163.com>
2020-02-14 14:40:33 +0800

    eslint

--- src/views/fundsData/Ledger.vue
+++ src/views/fundsData/Ledger.vue
@@ -96,33 +96,33 @@ export default {
         pickerOptionsStart () {
             return {
                 disabledDate: time => {
-                    let beginDateVal = this.queryParams.createTimeEnd;
+                    let beginDateVal = this.queryParams.createTimeEnd
                     if (beginDateVal) {
-                        return time.getTime() > new Date(beginDateVal).getTime();
+                        return time.getTime() > new Date(beginDateVal).getTime()
                     }
                 }
-            };
+            }
         },
         pickerOptionsEnd () {
             return {
                 disabledDate: time => {
                     let beginDateVal = this.queryParams.createTimeStart;
                     if (beginDateVal) {
-                        return time.getTime() < new Date(beginDateVal).getTime();
+                        return time.getTime() < new Date(beginDateVal).getTime()
                     }
                 }
-            };
+            }
         },
         isMultiple () {
-            return this.hosAuthCheck(this.reCheckAuth);
+            return this.hosAuthCheck(this.reCheckAuth)
         }
     },
     watch: {
         activeName (val) {
             if (val == "流贷") {
-                this.isTab = true;
+                this.isTab = true
             } else {
-                this.isTab = false;
+                this.isTab = false
             }
         }
     },
@@ -162,22 +162,22 @@ export default {
                 {
                     label: "月度滚动额（元）",
                     prop: "monthlyQuota",
-                    formatters: "money"
+                    formatters: 'money'
                 },
                 {
                     label: "应收账款扣减额（元）",
                     prop: "accountReceivableQuota",
-                    formatters: "money"
+                    formatters: 'money'
                 },
                 {
                     label: "初始实时用信额（元）",
                     prop: "dailyQuota",
-                    formatters: "money"
+                    formatters: 'money'
                 },
                 {
                     label: "实际实时用信额（元）",
                     prop: "realDailyQuota",
-                    formatters: "money"
+                    formatters: 'money'
                 },
                 { label: "本月利率(年化）", prop: "dailyInterestRate" },
                 { label: "创建日期", prop: "importDate" },
@@ -223,61 +223,56 @@ export default {
                                             : "el-icon-plus pointer"
                                     }
                                     onClick={() => {
-                                        this.handleExpand(scope, this.expandSell, 1);
+                                        this.handleExpand(scope, this.expandSell, 1)
                                     }}
                                 />
                             </span>
-                        );
+                        )
                     },
                     children: [
                         {
-                            prop: "salesIncomeIncludingTax",
-                            label: "销售收入（含税）/万",
-                            width: "150",
-                            displayAs: "money"
+                            prop: 'salesIncomeIncludingTax',
+                            label: '销售收入（含税）/万',
+                            width: '150',
+                            displayAs: 'money'
                         }
                     ]
                 }
             ]
-        };
+        }
     },
     mounted () {
-        this.onSearch();
+        this.onSearch()
     },
     methods: {
         handleExpand (scope, expandSellrr, num) {
             this.$set(
                 this.column[scope.$index],
-                "_expand",
+                '_expand',
                 !this.column[scope.$index]._expand
-            );
+            )
             if (this.column[scope.$index]._expand) {
-                this.column[scope.$index].children = this.column[
-                    scope.$index
-                ].children.concat(expandSellrr);
+                this.column[scope.$index].children = this.column[scope.$index].children.concat(expandSellrr)
             } else {
-                this.column[scope.$index].children = this.column[
-                    scope.$index
-                ].children.slice(0, num);
-                this.changeTable = false;
+                this.column[scope.$index].children = this.column[scope.$index].children.slice(0, num)
+                this.changeTable = false
                 this.$nextTick(() => {
-                    this.changeTable = true;
-                });
+                    this.changeTable = true
+                })
             }
         },
-        getList () { },
         onClose () {
-            this.dialogVisible = false;
+            this.dialogVisible = false
         },
         // 埋点
         tracking (event) {
-            this.$store.dispatch("tracking", {
+            this.$store.dispatch('tracking', {
                 type: 9,
                 event,
                 page: 2,
-                page_name: "额度导入",
-                page_path_name: "amountImport"
-            });
+                page_name: '额度导入',
+                page_path_name: 'amountImport'
+            })
         },
         onExportTemplate () {
             // 模板导出
@@ -289,32 +284,32 @@ export default {
         },
         onExportLedger () { }, // 台账导出
         handleClick () {
-            this.onSearch();
+            this.onSearch()
         },
         multiSelection (val) {
-            this.multiSelect = val;
+            this.multiSelect = val
         },
         isSuccess (response) {
             this.$message({
-                message: "批量导入成功！",
-                type: "success"
-            });
-            this.onSearch();
+                message: '批量导入成功！',
+                type: 'success'
+            })
+            this.onSearch()
         },
         isError (response) {
             this.$message({
-                message: "批量导入失败，" + JSON.parse(response.message).message,
-                type: "error"
-            });
+                message: '批量导入失败，' + JSON.parse(response.message).message,
+                type: 'error'
+            })
         },
         async onQuery () {
             // console.log(this.activeName + ' '  + this.produce)
-            this.searchParams.activeName = this.activeName;
-            this.searchParams.produce = this.produce;
+            this.searchParams.activeName = this.activeName
+            this.searchParams.produce = this.produce
             // console.log(this.searchParams)
             // const { data } = await getRateList(this.queryParams)
-            this.tableData = [];
-            this.tableData = [{ newShy: 11 }, { newShy: 22 }, { newShy: 33 }];
+            this.tableData = []
+            this.tableData = [{ newShy: 11 }, { newShy: 22 }, { newShy: 33 }]
             // this.pagination = {
             //     pageNumber: data.current,
             //     pageSize: data.size,
@@ -322,30 +317,30 @@ export default {
             // }
         },
         onSearch () {
-            this.searchParams = { ...this.queryParams };
-            this.onQuery();
+            this.searchParams = { ...this.queryParams }
+            this.onQuery()
         },
         onReset () {
-            this.$set(this.queryParams, "customerName", "");
-            this.$set(this.queryParams, "misCode", "");
-            this.$set(this.queryParams, "branch", "");
-            this.$set(this.queryParams, "ledger", "");
-            this.onSearch();
+            this.$set(this.queryParams, 'customerName', '')
+            this.$set(this.queryParams, 'misCode', '')
+            this.$set(this.queryParams, 'branch', '')
+            this.$set(this.queryParams, 'ledger', '')
+            this.onSearch()
         },
         getList (val) {
-            console.log(val);
+            console.log(val)
             this.searchParams = {
                 ...this.searchParams,
                 ...val
-            };
-            console.log(this.searchParams);
-            this.onQuery();
+            }
+            console.log(this.searchParams)
+            this.onQuery()
         },
         onLinddialog () {
-            this.$router.push({ path: "/fundsData/newFlowdialog" });
+            this.$router.push({ path: '/fundsData/newFlowdialog' })
         }
     }
-};
+}
 </script>
 
 <style lang='scss' scoped>
