7aa55fccec347c9412370d46215906cf5f2e7587
sunjun <990375133@qq.com>
2020-02-19 14:44:22 +0800

    HAM-7634 添加借款api

--- src/views/fundsData/components/dialog/supplierDialog.vue
+++ src/views/fundsData/components/dialog/supplierDialog.vue
@@ -33,10 +33,10 @@
                     </div>
                     <div class="query-cont-col">
                         <el-form-item label="借款期限： " prop="loanDateNum">
-                            <el-radio style="margin-right:5px" v-model.trim="loanDateType" label="月"></el-radio>
+                            <el-radio style="margin-right:5px" v-model.trim="form.loanDateType" :label="1"></el-radio>
                             <el-input v-model.trim="form.loanDateNum" v-isNum:0='form.loanDateNum' maxlength='3' placeholder="请输入借款期限"></el-input>
                             <span class="dw">月</span>
-                            <el-radio style="margin:0 5px 0 10px" v-model.trim="loanDateType" label="天"></el-radio>
+                            <el-radio style="margin:0 5px 0 10px" v-model.trim="form.loanDateType" :label="2"></el-radio>
                             <el-input v-model.trim="form.loanDateNum" v-isNum:0='form.loanDateNum' maxlength='3' placeholder="请输入借款期限"></el-input>
                             <span class="dw">天</span>
                         </el-form-item>
@@ -52,7 +52,7 @@
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button @click="onCancle">取 消</el-button>
-            <el-button type="primary" @click="onSure">保 存</el-button>
+            <el-button type="primary" @click="onSave">保 存</el-button>
         </span>
     </el-dialog>
 </template>
@@ -65,8 +65,6 @@ export default {
         return {
             loanDateType: '月',
             form: {
-                name: '',
-                description: '', // 台账借款表
                 accountId: '', // 台账id
                 depositPay: '', // 保证金缴纳
                 depositProportion: '', // 保证金比例
@@ -107,10 +105,28 @@ export default {
                 message: '修改成功'
             })
             this.onCancle()
+            this.$emit('reload')
         }
     },
     mounted () {
         console.log('supplierDialog:::detailData', this.detailData)
+        this.form = {
+            accountId: this.detailData.loan_accountId, // 台账id
+            depositPay: this.detailData.loan_depositPay, // 保证金缴纳
+            depositProportion: this.detailData.loan_depositProportion, // 保证金比例
+            id: this.detailData.loan_id, // 主键id
+            invoiceAmount: this.detailData.loan_invoiceAmount, // 开票金额
+            invoiceTime: this.detailData.loan_invoiceTime, // 开票时间
+            loanAmount: this.detailData.loan_loanAmount, // 借款金额（敞口金额）
+            loanDateNum: this.detailData.loan_loanDateNum, // 借款期限
+            loanDateType: this.detailData.loan_loanDateType, // 借款期限类型 1：月 2：天
+            loanEndTime: this.detailData.loan_loanEndTime, // 借款结束日期,格式为:yyyy-MM-dd HH:mm:ss
+            loanStartTime: this.detailData.loan_loanStartTime, // 借款开始日期
+            registrant: this.detailData.loan_registrant, // 登记人
+            repaymentType: this.detailData.loan_repaymentType, // 还款类型 1：一次性还款 2：334还款
+            supplier: this.detailData.loan_supplier, // 供货商
+            yearRate: this.detailData.loan_yearRate// 年利率
+        }
     }
 }
 </script>
