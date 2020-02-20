1162a76f0ee125b72499794a07df208777c0a295
shiyong <4227030122@qq.com>
2020-02-20 13:56:56 +0800

    HAM-7672 联调接口

--- src/views/fundsData/components/complexTable.vue
+++ src/views/fundsData/components/complexTable.vue
@@ -94,7 +94,7 @@ export default {
                             render: (h, scope) => {
                                 return <span>{scope.row.account_standingBookNo}<i class='el-icon-edit pointer' onClick={() => {
                                     this.getAccount(scope.row)
-                                    this.accountData.title = '好信用—流贷基础信息维护';
+                                    this.accountData.title = '好信用—流贷基础信息维护'
                                     this.misDialogVisible = true
                                 }}></i></span>
                             }
@@ -228,7 +228,7 @@ export default {
                             render: (h, scope) => {
                                 return <span>{scope.row.account_standingBookNo}<i class='el-icon-edit pointer' onClick={() => {
                                     this.getAccount(scope.row)
-                                    this.accountData.title = '好信用—分授信基础信息维护';
+                                    this.accountData.title = '好信用—分授信基础信息维护'
                                     this.misDialogVisible = true
                                 }}></i></span>
                             }
@@ -556,7 +556,7 @@ export default {
                             render: (h, scope) => {
                                 return <span>{scope.row.account_standingBookNo}<i class='el-icon-edit pointer' onClick={() => {
                                     this.getAccount(scope.row)
-                                    this.accountData.title = '好信用—敞口基础信息维护';
+                                    this.accountData.title = '好信用—敞口基础信息维护'
                                     this.misDialogVisible = true
                                 }}></i></span>
                             }
@@ -1345,7 +1345,13 @@ export default {
                     sort: 9,
                     width: '150',
                     render: (h, scope) => {
-                        return <span>{scope.row.loan_repaymentType == 0 ? 0 : scope.row.loan_repaymentType ? `${scope.row.loan_repaymentType}` : '-'}<i class='el-icon-edit pointer' onClick={() => { this.rowData = scope.row; this.rowData.title = '好信用—敞口还款信息维护'; this.repaymentDialogVisible = true }}></i></span>
+                        return <span>{scope.row.loan_repaymentType == 0 ? 0 : scope.row.loan_repaymentType ? `${scope.row.loan_repaymentType}` : '-'}<i class='el-icon-edit pointer'
+                            onClick={() => {
+                                this.getGrantPaymetPlanData(scope.row)
+                                this.rowData = scope.row
+                                this.rowData.title = '好信用—敞口还款信息维护222222'
+                                this.repaymentDialogVisible = true
+                            }}></i></span>
                     }
                 },
                 { prop: 'loan_registrant', label: '登记人', sort: 9, width: '150' }
@@ -1586,6 +1592,16 @@ export default {
         async getRespAccountRepaymentPlanData (row) {
             const { data } = await getRespAccountRepaymentPlan(row.plan_id)
             this.respAccountRepaymentPlanData = data
+        },
+        // 敞口还款
+        async getGrantPaymetPlanData (row) {
+            console.log(row)
+            const { data } = await getRespAccountRepaymentPlan(row.account_id)
+            this.rowData = [...data]
+            // this.rowData.title = '好信用—敞口还款信息维护3333'
+            this.$set(this.rowData[0], 'title', '好信用—敞口还款信息维护3333')
+            this.$set(this.rowData[0], 'repaymentType', row.loan_repaymentType)
+            console.log(this.rowData)
         }
     },
     mounted () {
