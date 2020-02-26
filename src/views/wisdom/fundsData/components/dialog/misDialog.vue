010e4bad1bbee99dfc90b9785934dbe319e9cc77
孙慧勇 <shy520134@163.com>
2020-02-26 16:50:35 +0800

    HAM-9039 必填项验证

--- src/views/wisdom/fundsData/components/dialog/misDialog.vue
+++ src/views/wisdom/fundsData/components/dialog/misDialog.vue
@@ -17,12 +17,12 @@
                     </div>
                     <div class="query-cont-col">
                         <el-form-item label="MIS编码：" prop="misCode">
-                            <span>{{detailData.misCode}}</span>
+                            <span class="w150">{{detailData.misCode}}</span>
                         </el-form-item>
                     </div>
                     <div class="query-cont-col">
-                        <el-form-item label="分部：" prop="misCode">
-                            <span>{{detailData.subsectionName}}</span>
+                        <el-form-item label="分部：" prop="subsectionName">
+                            <span class="w150">{{detailData.subsectionName}}</span>
                         </el-form-item>
                     </div>
                 </div>
@@ -52,6 +52,15 @@ export default {
             rules: {
                 standingBookNo: [
                     { required: true, message: '请输入台账编号', trigger: 'blur' }
+                ],
+                loanCompanyName: [
+                    { required: true, message: '请输入借款单位', trigger: 'blur' }
+                ],
+                misCode: [
+                    { required: true, message: '请输入misCode', trigger: 'blur' }
+                ],
+                subsectionName: [
+                    { required: true, message: '请输入分部', trigger: 'blur' }
                 ]
             },
             loading: false
@@ -118,5 +127,9 @@ export default {
 <style lang="scss" scoped>
 /deep/ .el-dialog__body {
     padding: 20px 24px;
+}
+.w150{
+    width: 150px;
+    display: inline-block;
 }
 </style>
