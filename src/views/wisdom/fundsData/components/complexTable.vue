8fc4986079a407b9f50f485661fdc2dc5cf45fba
shiyong <4227030122@qq.com>
2020-02-27 15:25:46 +0800

    HAM-9059 放款日未维护时间问题

--- src/views/wisdom/fundsData/components/complexTable.vue
+++ src/views/wisdom/fundsData/components/complexTable.vue
@@ -1658,12 +1658,12 @@ export default {
             }
             if (data.loanDateType == 1) {
                 this.$set(this.loanData, 'loanDateNumM', data.loanDateNum)
-                this.loanData.loanEndTimeLoan = moment(data.loanStartTime).add(data.loanDateNum, 'M').format('YYYY-MM-DD')
+                this.loanData.loanEndTimeLoan = data.loanStartTime && moment(data.loanStartTime).add(data.loanDateNum, 'M').format('YYYY-MM-DD')
                 this.loanData.loanEndTimeInvoice = data.invoiceTime ? moment(data.invoiceTime).add(data.loanDateNum, 'M').format('YYYY-MM-DD') : '-'
             }
             if (data.loanDateType == 2) {
                 this.$set(this.loanData, 'loanDateNumD', data.loanDateNum)
-                this.loanData.loanEndTimeLoan = moment(data.loanStartTime).add(data.loanDateNum, 'd').format('YYYY-MM-DD')
+                this.loanData.loanEndTimeLoan = data.loanStartTime && moment(data.loanStartTime).add(data.loanDateNum, 'd').format('YYYY-MM-DD')
                 this.loanData.loanEndTimeInvoice = data.invoiceTime ? moment(data.invoiceTime).add(data.loanDateNum, 'd').format('YYYY-MM-DD') : '-'
             }
             if (!data.loanDateType) {
