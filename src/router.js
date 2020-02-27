0185023a8068e44dd96ae23f05809b6746b5975d
shiyong <4227030122@qq.com>
2020-02-27 09:13:31 +0800

    代码合并

--- src/router.js
+++ src/router.js
@@ -234,41 +234,7 @@ const routerMapping = [
             }
         ]
     },
-    {
-        path: '/fundsData',
-        meta: {
-            title: '好智慧-资金数据',
-            isMenu: true,
-            icon: 'hosjoy_operation'
-        },
-        component: Layout,
-        children: [
-            {
-                path: 'standingBook',
-                name: 'standingBook',
-                meta: {
-                    title: '资金借款台账',
-                    tagName: '资金借款台账',
-                    parentName: '好智慧-资金数据',
-                    isMenu: true,
-                    icon: ''
-                },
-                component: () => import('@/views/wisdom/fundsData/Ledger.vue')
-            },
-            {
-                path: 'newFlowdialog',
-                name: 'newFlowdialog',
-                meta: {
-                    title: '新增台账',
-                    tagName: '新增台账',
-                    parentName: '好智慧-资金数据',
-                    isMenu: false,
-                    icon: ''
-                },
-                component: () => import('@/views/wisdom/fundsData/components/dialog/newFlowdialog.vue')
-            }
-        ]
-    },
+   
     {
         path: '/comfortCloud',
         meta: {
--- src/router/Wisdomrouter.js
+++ src/router/Wisdomrouter.js
@@ -1,112 +1,149 @@
 import Layout from '@/views/layout/Default.vue'
 
-export default {
-    path: '/wisdom',
-    meta: {
-        title: '好智慧-经营分析',
-        isMenu: true,
-        icon: 'hosjoy_operation'
+export default [
+    {
+        path: '/fundsData',
+        meta: {
+            title: '好智慧-资金数据',
+            isMenu: true,
+            icon: 'hosjoy_operation'
+        },
+        component: Layout,
+        children: [
+            {
+                path: 'standingBook',
+                name: 'standingBook',
+                meta: {
+                    title: '资金借款台账',
+                    tagName: '资金借款台账',
+                    parentName: '好智慧-资金数据',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/wisdom/fundsData/Ledger.vue')
+            },
+            {
+                path: 'newFlowdialog',
+                name: 'newFlowdialog',
+                meta: {
+                    title: '新增台账',
+                    tagName: '新增台账',
+                    parentName: '好智慧-资金数据',
+                    isMenu: false,
+                    icon: ''
+                },
+                component: () => import('@/views/wisdom/fundsData/components/dialog/newFlowdialog.vue')
+            }
+        ]
     },
-    // redirect: '/wisdom/' + this.children[0].path,
-    component: Layout,
-    children: [
-        {
-            path: 'manageoverview',
-            name: 'manageoverview',
-            meta: {
-                title: '经营概览',
-                tagName: '经营概览',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/manageoverview'
-            },
-            component: () => import('@/views/wisdom/manageoverview')
+    {
+        path: '/wisdom',
+        meta: {
+            title: '好智慧-经营分析',
+            isMenu: true,
+            icon: 'hosjoy_operation'
         },
-        {
-            path: 'branchTarget',
-            name: 'branchTarget',
-            meta: {
-                title: '分部目标管理',
-                tagName: '分部目标管理',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/branchTarget'
+        // redirect: '/wisdom/' + this.children[0].path,
+        component: Layout,
+        children: [
+            {
+                path: 'manageoverview',
+                name: 'manageoverview',
+                meta: {
+                    title: '经营概览',
+                    tagName: '经营概览',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/manageoverview'
+                },
+                component: () => import('@/views/wisdom/manageoverview')
             },
-            component: () => import('@/views/wisdom/branchTarget')
-        },
-        {
-            path: 'platformTarget',
-            name: 'platformTarget',
-            meta: {
-                title: '平台目标管理',
-                tagName: '平台目标管理',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/platformTarget'
+            {
+                path: 'branchTarget',
+                name: 'branchTarget',
+                meta: {
+                    title: '分部目标管理',
+                    tagName: '分部目标管理',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/branchTarget'
+                },
+                component: () => import('@/views/wisdom/branchTarget')
             },
-            component: () => import('@/views/wisdom/platformTarget')
-        },
+            {
+                path: 'platformTarget',
+                name: 'platformTarget',
+                meta: {
+                    title: '平台目标管理',
+                    tagName: '平台目标管理',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/platformTarget'
+                },
+                component: () => import('@/views/wisdom/platformTarget')
+            },
 
-        {
-            path: 'platcompanyinfo',
-            name: 'platcompanyinfo',
-            meta: {
-                title: '平台公司基本信息',
-                tagName: '平台公司基本信息',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/platcompanyinfo'
+            {
+                path: 'platcompanyinfo',
+                name: 'platcompanyinfo',
+                meta: {
+                    title: '平台公司基本信息',
+                    tagName: '平台公司基本信息',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/platcompanyinfo'
+                },
+                component: () => import('@/views/wisdom/platcompanyinfo')
             },
-            component: () => import('@/views/wisdom/platcompanyinfo')
-        },
 
-        {
-            path: 'platformSale',
-            name: 'platformSale',
-            meta: {
-                title: '平台销售分析',
-                tagName: '平台销售分析',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/platformSale'
+            {
+                path: 'platformSale',
+                name: 'platformSale',
+                meta: {
+                    title: '平台销售分析',
+                    tagName: '平台销售分析',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/platformSale'
+                },
+                component: () => import('@/views/wisdom/platformSale')
             },
-            component: () => import('@/views/wisdom/platformSale')
-        },
-        {
-            path: 'branchSale',
-            name: 'branchSale',
-            meta: {
-                title: '分部销售分析',
-                tagName: '分部销售分析',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/branchSale'
+            {
+                path: 'branchSale',
+                name: 'branchSale',
+                meta: {
+                    title: '分部销售分析',
+                    tagName: '分部销售分析',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/branchSale'
+                },
+                component: () => import('@/views/wisdom/branchSale')
             },
-            component: () => import('@/views/wisdom/branchSale')
-        },
-        {
-            path: 'categorySale',
-            name: 'categorySale',
-            meta: {
-                title: '品类销售分析',
-                tagName: '品类销售分析',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/categorySale'
+            {
+                path: 'categorySale',
+                name: 'categorySale',
+                meta: {
+                    title: '品类销售分析',
+                    tagName: '品类销售分析',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/categorySale'
+                },
+                component: () => import('@/views/wisdom/categorySale')
             },
-            component: () => import('@/views/wisdom/categorySale')
-        },
-        {
-            path: 'profitStatistics',
-            name: 'profitStatistics',
-            meta: {
-                title: '利润统计',
-                tagName: '利润统计',
-                isMenu: true,
-                icon: '',
-                component: '@/views/wisdom/profitStatistics'
-            },
-            component: () => import('@/views/wisdom/profitStatistics')
-        }
-    ]
-}
+            {
+                path: 'profitStatistics',
+                name: 'profitStatistics',
+                meta: {
+                    title: '利润统计',
+                    tagName: '利润统计',
+                    isMenu: true,
+                    icon: '',
+                    component: '@/views/wisdom/profitStatistics'
+                },
+                component: () => import('@/views/wisdom/profitStatistics')
+            }
+        ]
+    }
+]
