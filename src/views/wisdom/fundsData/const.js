3f5ef21f7cf81904dc913dc9533544603be62fe9
sunhuiyong <1349824311@qq.com>
2020-03-07 10:42:59 +0800

    HAM-9168

---
+++ src/views/wisdom/fundsData/const.js
@@ -0,0 +1,10 @@
+// 1：流贷 2：敞口 3：分授信 4：还款明细表
+const accountName = ['流贷', '敞口', '分授信', '']
+// 1：好信用 2：供应链 3：好橙工
+const productName = ['好信用', '供应链', '好橙工']
+
+
+export {
+    accountName,
+    productName
+}
