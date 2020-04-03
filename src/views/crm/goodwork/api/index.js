a2bd408ad1dafbc9aebf0ef8feffcf3458e10e45
shiyong <shiyong123>
2020-04-03 11:10:37 +0800

    HAM-10446 同一网关

--- src/views/crm/goodwork/api/index.js
+++ src/views/crm/goodwork/api/index.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
-import { crmUrl } from '@/api/config'
 //
 export function getProject (params) {
-    return axios.get(crmUrl + `/api/project/page`, { params })
+    return axios.get(`/memeber/api/project/page`, { params })
 }
 
-export const getProjectDetail = (params) => axios.get(crmUrl + `/api/project/${params}`)
+export const getProjectDetail = (params) => axios.get(`/memeber/api/project/${params}`)
 
-export const putProjectDetail = (params) => axios.put(crmUrl + `/api/project`, params)
+export const putProjectDetail = (params) => axios.put(`/memeber/api/project`, params)
 
-export const saveStatus = (params) => axios.post(crmUrl + `/api/project/status`, params)
+export const saveStatus = (params) => axios.post(`/memeber/api/project/status`, params)
 
-export const updateAudit = (params) => axios.post(crmUrl + `/api/project/audit`, params)
+export const updateAudit = (params) => axios.post(`/memeber/api/project/audit`, params)
