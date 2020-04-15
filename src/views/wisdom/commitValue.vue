862821ce744c82057d8e1d0c37a45b405b458835
shy <1349824311@qq.com>
2020-04-15 17:37:21 +0800

    eslint

--- src/views/wisdom/commitValue.vue
+++ src/views/wisdom/commitValue.vue
@@ -76,7 +76,7 @@ export default {
                 platformObj: {
                     selectCode: '',
                     selectName: ''
-                },
+                }
             },
             queryParams: {
                 regionCode: '',
