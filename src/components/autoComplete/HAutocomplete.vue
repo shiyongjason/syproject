606cdbfb87ad82effa04037da64834f2d71c8b97
shy <1349824311@qq.com>
2020-04-27 09:42:45 +0800

    HAM-11697 间隔时间增加至500毫秒

--- src/components/autoComplete/HAutocomplete.vue
+++ src/components/autoComplete/HAutocomplete.vue
@@ -95,7 +95,7 @@ export default {
                 this.$emit('back-event', {
                     value: item
                 })
-            }, 200)
+            }, 500)
         },
         blurInput (item) {
             if (!this.canDoBlurMethos) {
