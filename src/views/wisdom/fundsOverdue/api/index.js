659d270017284d99aeac2854163c7514a55faf0e
shy <1349824311@qq.com>
2020-05-12 15:01:08 +0800

    HAM-11802 平台公司导入

--- src/views/wisdom/fundsOverdue/api/index.js
+++ src/views/wisdom/fundsOverdue/api/index.js
@@ -4,16 +4,23 @@ export const getCompanyOverdueList = (params) => axios.get(`backend/api/company/
 // 平台公司逾期汇总合计
 export const getCompanyOverdueListTotal = (params) => axios.get(`backend/api/company/overdue/summary/total`, { params })
 
-// 下载故障模板
-export function downloadPlanTotalList (params) {
+// 平台公司逾期汇总导出
+export function exportCompanyOverdueExcel (params) {
     axios.defaults.responseType = 'blob'
-    axios.get(`backend/api/fund-plan/summary/export`, { params }).then(function (response) {
+    axios.get(`backend/api/company/overdue/summary/export`, { params }).then(function (response) {
         try {
             const reader = new FileReader()
             reader.readAsDataURL(response.data)
             reader.onload = function (e) {
+                console.log(params)
                 const a = document.createElement('a')
-                a.download = '计划汇总分析表.xlsx'
+                let name = ''
+                if (params.state == 1) {
+                    name = '存量'
+                } else {
+                    name = '增量'
+                }
+                a.download = `平台公司逾期${name}汇总表.xlsx`
                 a.href = e.target.result
                 document.querySelector('body').appendChild(a)
                 a.click()
