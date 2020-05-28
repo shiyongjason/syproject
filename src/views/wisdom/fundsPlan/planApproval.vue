dc5711ca4dd9ee17f6e4f361f21fde0530f447f5
yosun <git@yosun.com>
2020-05-28 11:30:03 +0800

    HAM-12580 资金审批额度列表接口调试

--- src/views/wisdom/fundsPlan/planApproval.vue
+++ src/views/wisdom/fundsPlan/planApproval.vue
@@ -34,9 +34,8 @@
                     <el-button type="primary" class="ml20" @click="onReset">
                         重置
                     </el-button>
-                    <el-button type="primary" class="ml20" @click="onExport">
-                        导出汇总表
-                    </el-button>
+                    <el-button type="primary" class="ml20" @click="onShowImport">导入表格</el-button>
+                    <el-button type="primary" class="ml20" @click="onExport">导出表格</el-button>
                 </div>
             </div>
         </div>
@@ -51,6 +50,27 @@
                 </template>
             </hosJoyTable>
         </div>
+        <el-dialog title="冲刺值利润指标导入" :visible.sync="dialogFormVisible" center :close-on-click-modal='false'>
+            <el-form :model="uploadData" :rules="rules" ref="form">
+                <el-form-item label="导入模板下载：" label-width="200px">
+                    <a class="downloadExcel" href="/excelTemplate/承诺值导入模板.xls" download="承诺值导入模板.xls">
+                        冲刺值利润指标导入模板导出
+                    </a>
+                </el-form-item>
+                <el-form-item label="请选择导入年份：" label-width="200px" prop='commitmentYear'>
+                    <el-date-picker v-model="uploadData.commitmentYear" type="year" value-format='yyyy' placeholder="选择年" :editable='false' :clearable='false'>
+                    </el-date-picker>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-upload class="upload-demo" :show-file-list="false" :action="interfaceUrl + '/api/overdue/annual/target/profit/import'" :on-success="isSuccess" :on-error="isError" :before-upload="handleUpload" auto-upload :headers='headersData' :data='uploadData'>
+                    <el-button type="primary" class='m0' :loading='loading'>
+                        导入表格
+                    </el-button>
+                </el-upload>
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -60,6 +80,7 @@ import hosJoyTable from '@/components/HosJoyTable/hosjoy-table'
 import { departmentAuth } from '@/mixins/userAuth'
 import { mapActions, mapGetters, mapState } from 'vuex'
 import { platformPlan } from './const'
+import { interfaceUrl } from '@/api/config'
 export default {
     name: 'planApproval',
     mixins: [departmentAuth],
@@ -116,10 +137,29 @@ export default {
             },
             page: {
                 total: 0
-            }
+            },
+            dialogFormVisible: false,
+            uploadData: {
+                commitmentYear: ''
+            },
+            rules: {
+                commitmentYear: [
+                    { required: true, message: '请选择年', trigger: 'blur' }
+                ]
+            },
+            interfaceUrl: interfaceUrl,
+            headersData: {
+                'refreshToken': sessionStorage.getItem('refreshToken'),
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            },
+            loading: false,
+            accept: '.xlsx,.xls'
         }
     },
-    mounted () {
+    async mounted () {
+        await this.findTargetTime()
+        // console.log(this.targetTime)
+        this.params.valueYear = this.targetTime
         this.findPlanApprovalList(this.params)
     },
     methods: {
@@ -190,11 +230,56 @@ export default {
         onExport () {
 
         },
+        onShowImport () {
+            this.dialogFormVisible = true
+            this.$nextTick(() => {
+                this.$refs['form'].clearValidate()
+            })
+        },
         onReset () {
 
         },
+        handleUpload (file) {
+            this.$refs['form'].validate((valid) => { })
+            if (!this.uploadData.commitmentYear) {
+                this.$message({
+                    message: '请先选择导入年份！',
+                    type: 'warning'
+                })
+                return false
+            }
+            if (file.size / (1024 * 1024) > 100) {
+                this.$message({
+                    message: '附件要保持100M以内',
+                    type: 'warning'
+                })
+                return false
+            }
+            const fileSuffix = file.name.substring(file.name.lastIndexOf('.'))
+            if (this.accept.lastIndexOf(fileSuffix) == -1) {
+                this.$message.error('格式不正确！')
+                return false
+            }
+            this.loading = true
+        },
+        isSuccess () {
+            this.$message({
+                message: '批量导入成功！',
+                type: 'success'
+            })
+            this.loading = false
+            this.onSearch()
+        },
+        isError (response) {
+            this.$message({
+                message: '批量导入失败，' + JSON.parse(response.message).message,
+                type: 'error'
+            })
+            this.loading = false
+        },
         ...mapActions({
-            findPlanApprovalList: 'fundsPlan/findPlanApprovalList'
+            findPlanApprovalList: 'fundsPlan/findPlanApprovalList',
+            findTargetTime: 'fundsPlan/findTargetTime'
         })
     }
 }
