af044bd86b78e55808b959faed5611fe32376d68
yosun <git@yosun.com>
2020-06-11 11:41:03 +0800

    HAM-13496 分部联动修改

--- src/components/autoComplete/HAutocomplete.vue
+++ src/components/autoComplete/HAutocomplete.vue
@@ -95,7 +95,7 @@ export default {
                 this.$emit('back-event', {
                     value: item
                 })
-            }, 80)
+            }, 300)
         },
         blurInput (item) {
             if (!this.canDoBlurMethos) {
