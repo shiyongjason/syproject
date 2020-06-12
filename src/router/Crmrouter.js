fbe0a171ec27f4194c8aa095affb226b378e0964
sunhuiyong <1349824311@qq.com>
2020-06-12 15:49:42 +0800

    冲突

--- src/router/Crmrouter.js
+++ src/router/Crmrouter.js
@@ -1,79 +1,81 @@
 import Layout from '@/views/layout/Default.vue'
 
-export default {
-    path: '/goodwork',
-    meta: {
-        title: '好橙工',
-        isMenu: true,
-        icon: 'hosjoy_hcg'
-    },
-    component: Layout,
-    children: [
-        {
-            path: 'projectlist',
-            name: 'projectlist',
-            meta: {
-                title: '好橙工项目',
-                tagName: '好橙工项目',
-                isMenu: true,
-                icon: ''
-            },
-            component: () => import('@/views/crm/goodwork/projectlist')
+export default [
+    {
+        path: '/goodwork',
+        meta: {
+            title: '好橙工',
+            isMenu: true,
+            icon: 'hosjoy_hcg'
         },
-        {
-            path: 'authenlist',
-            name: 'authenlist',
-            meta: {
-                title: '企业管理',
-                tagName: '企业管理',
-                isMenu: true,
-                icon: ''
+        component: Layout,
+        children: [
+            {
+                path: 'projectlist',
+                name: 'projectlist',
+                meta: {
+                    title: '好橙工项目',
+                    tagName: '好橙工项目',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/crm/goodwork/projectlist')
             },
-            component: () => import('@/views/crm/crmauthen/authenlist')
-        },
-        {
-            path: 'staticlist',
-            name: 'staticlist',
-            meta: {
-                title: '好橙工台账',
-                tagName: '好橙工台账',
-                isMenu: true,
-                icon: ''
+            {
+                path: 'authenlist',
+                name: 'authenlist',
+                meta: {
+                    title: '企业管理',
+                    tagName: '企业管理',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/crm/crmauthen/authenlist')
             },
-            component: () => import('@/views/crm/crmstatic/staticlist')
-        },
-        {
-            path: 'applicationList',
-            name: 'applicationList',
-            meta: {
-                title: '好橙工VIP申请列表',
-                tagName: '好橙工VIP申请列表',
-                isMenu: true,
-                icon: ''
+            {
+                path: 'staticlist',
+                name: 'staticlist',
+                meta: {
+                    title: '好橙工台账',
+                    tagName: '好橙工台账',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/crm/crmstatic/staticlist')
             },
-            component: () => import('@/views/crm/vipapplication/applicationList')
-        },
-        {
-            path: 'vipmanlist',
-            name: 'vipmanlist',
-            meta: {
-                title: '好橙工VIP管理列表',
-                tagName: '好橙工VIP管理列表',
-                isMenu: true,
-                icon: ''
+            {
+                path: 'applicationList',
+                name: 'applicationList',
+                meta: {
+                    title: '好橙工VIP申请列表',
+                    tagName: '好橙工VIP申请列表',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/crm/vipapplication/applicationList')
             },
-            component: () => import('@/views/crm/vipmanage/vipmanlist')
-        },
-        {
-            path: 'creditmanlist',
-            name: 'creditmanlist',
-            meta: {
-                title: '好橙工信用管理列表',
-                tagName: '好橙工信用管理列表',
-                isMenu: true,
-                icon: ''
+            {
+                path: 'vipmanlist',
+                name: 'vipmanlist',
+                meta: {
+                    title: '好橙工VIP管理列表',
+                    tagName: '好橙工VIP管理列表',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/crm/vipmanage/vipmanlist')
             },
-            component: () => import('@/views/crm/creditmanage/creditmanlist')
-        }
-    ]
-}
+            {
+                path: 'creditmanlist',
+                name: 'creditmanlist',
+                meta: {
+                    title: '好橙工信用管理列表',
+                    tagName: '好橙工信用管理列表',
+                    isMenu: true,
+                    icon: ''
+                },
+                component: () => import('@/views/crm/creditmanage/creditmanlist')
+            }
+        ]
+    }
+]
