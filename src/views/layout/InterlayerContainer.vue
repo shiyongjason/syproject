38e3ff1e5bdf8ec7efa2e490cb0a2e78fc2d43a9
sunhuiyong <1349824311@qq.com>
2020-06-15 18:13:13 +0800

    BTB-16056 三级菜单bug

--- src/views/layout/InterlayerContainer.vue
+++ src/views/layout/InterlayerContainer.vue
@@ -1,10 +1,18 @@
 <template>
-    <router-view></router-view>
+    <keep-alive :include="cachedInclude" :exclude="cachedExclude">
+        <router-view></router-view>
+    </keep-alive>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-
+    computed: {
+        ...mapState({
+            cachedInclude: state => state.cachedInclude,
+            cachedExclude: state => state.cachedExclude
+        })
+    }
 }
 </script>
 
