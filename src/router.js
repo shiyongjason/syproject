812072ebdadcb850956a431ea95e8c72f07f93d7
sunhuiyong <1349824311@qq.com>
2020-06-15 14:35:00 +0800

    BTB-16056 三级菜单

--- src/router.js
+++ src/router.js
@@ -326,7 +326,7 @@ router.beforeEach(async (to, from, next) => {
         })
     }
     // TODO 获取B2b token 项目路径 hmall（重新获取token）
-    if (to.path.indexOf('hmall') > 0 || to.path.indexOf('paymentCentral') > 0|| to.path.indexOf('goodwork') > 0){
+    if (to.path.indexOf('b2b') > 0 || to.path.indexOf('paymentCentral') > 0|| to.path.indexOf('goodwork') > 0){
         // 登录token带到请求的头部中，用于校验登录状态
         const token = sessionStorage.getItem('tokenB2b')
         if (token) {
