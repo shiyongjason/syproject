18cd2146e62155ebff791f31893ea13e7faa768a
maguangcheng <599015274@qq.com>
2020-06-18 16:11:08 +0800

    IOT-7326 BOSS-知识库管理，编辑知识页面，点击添加视频，不选择任何视频文件后确定，能正常添加和保存

--- src/views/comfortcloud/knowledge/knowledgeEdit.vue
+++ src/views/comfortcloud/knowledge/knowledgeEdit.vue
@@ -198,7 +198,7 @@ export default {
         },
         onInsertVideo () {
             if (!this.uploadedUrl) {
-                this.$message.error('请选择上传视频');
+                this.$message.error('请选择上传视频')
                 return false
             }
             this.$refs.editors.onInsertUrl(`</br><video src="${this.uploadedUrl}"  poster="" controls controlsList="nofullscreen nodownload noremote footbar" width="450" height="300" style="border:1px solid #f5f5f5;"></video></br>`)
