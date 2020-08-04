2dc5500f4854682c20fbb038ded8af7e3d05fc05
sunjun <990375133@qq.com>
2020-08-04 09:34:36 +0800

    fix hosjoyAutoCompleteRbp

--- src/components/HosjoyAutoCompleteRbp/hosjoyAutoCompleteRbp.vue
+++ src/components/HosjoyAutoCompleteRbp/hosjoyAutoCompleteRbp.vue
@@ -162,6 +162,7 @@ export default {
             const results = this.selectArray && this.selectArray.filter(v => {
                 return (v.value === item.target.value)
             })
+            console.log('results: ', results)
             if (results.length == 0) {
                 if (this.whichInput === 'D') {
                     this.choosedItem.regionName = ''
@@ -174,10 +175,10 @@ export default {
                     // this.doBlurMethods && this.findAuthList({ deptType: 'F', pkDeptDoc: this.userInfo.pkDeptDoc }) // watch会触发
                 }
                 if (this.whichInput === 'F') {
-                    if (!this.platCompanyDataSync) {
-                        this.platCompanyDataSync = ''
-                        this.choosedItem.platCompany = ''
-                    }
+                    this.platCompanyDataSync = ''
+                    this.choosedItem.platCompany = ''
+                    this.branchDataSync = ''
+                    this.choosedItem.branchName = ''
                     this.doBlurMethods && this.findPlatformslist()
                 }
                 if (this.whichInput === 'P') {
