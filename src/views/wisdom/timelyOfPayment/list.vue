2a4bd1efe671604b966088194bbc885a25250d95
shiyong <shiyong123>
2020-08-06 18:02:25 +0800

    冒烟测试用例

--- src/views/wisdom/timelyOfPayment/list.vue
+++ src/views/wisdom/timelyOfPayment/list.vue
@@ -26,7 +26,7 @@
                         </div>
                     </div> -->
                 <div class="query-cont-col flex-box-time">
-                    <div class="query-col-title">年份：</div>
+                    <div class="query-col-title">查询时间：</div>
                     <el-date-picker v-model="queryParams.selectDate" type="date" value-format='yyyy-MM-dd' placeholder="选择年" :editable='false' :clearable='false'>
                     </el-date-picker>
                 </div>
