9c2dac2cf16d07be31958aa6bfac0763af94a6ab
shiyong <shiyong123>
2020-09-01 20:40:41 +0800

    分号问题

--- src/views/crm/wechatcustomer/list.vue
+++ src/views/crm/wechatcustomer/list.vue
@@ -64,7 +64,7 @@
             </div>
         </div>
         <div class="page-body-cont">
-            <el-tag size="medium" class="eltagtop">已筛选 {{tableLoan.totalNum||0}},已注册：{{tableLoan.registerUserNum||0}},未注册:{{tableLoan.waitRegisterUserNum||0}}</el-tag>
+            <el-tag size="medium" class="eltagtop">已筛选 {{tableLoan.totalNum||0}},已注册:{{tableLoan.registerUserNum||0}},未注册:{{tableLoan.waitRegisterUserNum||0}}</el-tag>
             <hosJoyTable isShowIndex ref="hosjoyTable" align="center" collapseShow border stripe showPagination :column="tableLabel" :data="tableData" :pageNumber.sync="queryParams.pageNumber" :pageSize.sync="queryParams.pageSize" :total="paginationInfo.total" @pagination="searchList"
                 actionWidth='300' isAction :isActionFixed='tableData&&tableData.length>0' @sort-change='sortChange' isSimpleTable :localName="'v3.5.0'">
                 <template slot="action" slot-scope="scope">
