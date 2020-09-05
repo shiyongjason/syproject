e54a8b491f27b6c69300b8fb203053303c6cdc44
sunjun <990375133@qq.com>
2020-09-05 15:24:47 +0800

    deepclone

--- src/views/layout/Tags.vue
+++ src/views/layout/Tags.vue
@@ -190,14 +190,13 @@ export default {
                 }
             }
             this.setTags(newValue)
-        }
-        /*,
+        },
         cachedInclude (val) {
             sessionStorage.setItem('cachedInclude', val)
         },
         cachedExclude (val) {
             sessionStorage.setItem('cachedExclude', val)
-        } */
+        }
     },
     mounted () {
         let tags = []
