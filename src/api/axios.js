c8b4edb91add97cd20a7b2ee1522faae462e35df
wangqi <wangqi@hosjoy.com>
2020-10-10 15:20:54 +0800

    HAM-18607 token失效问题修改

--- src/api/axios.js
+++ src/api/axios.js
@@ -124,7 +124,6 @@ axios.interceptors.response.use(
                         window.location.href = '/login'
                         clearTimeout(timer)
                     }, 1200)
-                    throw Error('权限无效')
                 }
             } else {
                 if (error.response.status === 400 && data.message !== '') {
