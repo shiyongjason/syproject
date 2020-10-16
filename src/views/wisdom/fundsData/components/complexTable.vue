373ed49abf76a0ae06fda017d0c71787b44462d7
wangqi <wangqi@hosjoy.com>
2020-10-16 11:31:01 +0800

    HAM-18643 bug修复

--- src/views/wisdom/fundsData/components/complexTable.vue
+++ src/views/wisdom/fundsData/components/complexTable.vue
@@ -3787,7 +3787,6 @@ export default {
                             val.label = String(filters.fundMoneyHaveSpot(newTableDataTotal[val.prop || val.selfProp]))
                         }
                     } else {
-                        console.log(val)
                         if (val.children) {
                             if (val.children[0].label !== '合计') {
                                 val.children[0].label = '-'
