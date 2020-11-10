6fae15da4e413df229a9991dcd6e9c8d38326397
shiyong <shiyong123>
2020-11-10 20:25:25 +0800

    HAM-19204  【前端】合同模板预研

--- src/views/crm/contractTemp/api/index.js
+++ src/views/crm/contractTemp/api/index.js
@@ -1,6 +1,17 @@
 import axios from 'axios'
 const URL = 'http://192.168.20.248:30100'
-export const getContratTemp = (params) => axios.get(URL + `/api/contract-templates`, { params })
+export const getContratTemp = (params) => axios.get(`/project/api/contract-templates`, { params })
 
 // 合同模板详情
-export const getContratDetail = (params) => axios.get(URL + `/api/contract-templates/{params}`)
+export const getContratDetail = (params) => axios.get(`/project/api/contract-templates/{params}`)
+
+// 模板type
+export const getContratType = (params) => axios.get(`/project/api/contract-template/types`)
+
+// 合同约定字段
+export const getContratParam = (params) => axios.get(`/project/api/contract-template/types/${params}/params`)
+
+// 新增模板
+export const addContractTemp = (params) => axios.post(`/project/api/contract-templates`, params)
+
+export const getAllparams = (params) => axios.get(`/project/api/contract-template/types/${params}/params`)
