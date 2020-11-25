1b5f7794d51ba64cdec11a165482deb5a2d481d3
shiyong <shiyong123>
2020-11-25 16:18:40 +0800

    HAM-20626  BOSS：添加签署方弹框中签署方名称问题

--- src/views/crm/contractTemp/components/contractDialog.vue
+++ src/views/crm/contractTemp/components/contractDialog.vue
@@ -3,7 +3,7 @@
         <el-dialog :title="`添加${contractType==1?'平台':''}签署方`" :visible.sync="tract_visible" width="35%" :before-close="handleClose" :close-on-click-modal=false>
             <el-form :model="signerTempForm" :rules="signerTempFormrules" ref="signerTempR" label-width="140px" class="demo-signerTempForm" v-if="contractType==2">
                 <el-form-item label="签署方名称：" prop="signerName">
-                    <el-input v-model="signerTempForm.signerName" maxlength="50"></el-input>
+                    <el-input v-model="signerTempForm.signerName" placeholder="请输入" maxlength="50"></el-input>
                 </el-form-item>
                 <el-form-item label="签署方类型：" prop="signerType">
                     <el-radio-group v-model="signerTempForm.signerType" :disabled=isEdit @change="changeRadio">
