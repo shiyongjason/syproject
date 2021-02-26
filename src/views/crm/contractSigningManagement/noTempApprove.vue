089cdd1b476386beffb10734dff17eafeb8e06e6
shiyong <422703012@qq.com>
2021-02-26 15:53:11 +0800

    HAM-23674 【前端】支付单管理审核

--- src/views/crm/contractSigningManagement/noTempApprove.vue
+++ src/views/crm/contractSigningManagement/noTempApprove.vue
@@ -3,12 +3,12 @@
         <div class="page-body-cont">
             <div class="contract-flex">
                 <div>
-                    <h-button type="primary">下载合同</h-button>
+                    <h-button type="primary">下载原始合同</h-button>
                 </div>
                 <p>这个合同.jpg</p>
                 <p>这个合同.jpg</p>
                 <div class="contract-flex_bot">
-                    <h-button type="primary" @click="onResive">驳回</h-button>
+                    <h-button type="primary" @click="onResive">驳回合同</h-button>
                 </div>
             </div>
             <div class="contract-flex">
@@ -17,22 +17,24 @@
                         <h-button type="primary">上传合同</h-button>
                     </hosjoyUpload>
                 </div>
-                {{contractList}}
-                <div v-for="(item,index) in contractList" :key="index" class="contract-flex_preview">{{item.fieldName}}
-                    <div>预览</div>
-                    <div>下载</div>
+                <div class="contract-flex_tips">（请先点击上方“下载原始合同”，下载后在本地进行编辑修订，修订完成后在此上传，上传格式为.doc/.docx/.xls/.xlsx/.pdf等格式)</div>
+                <div v-for="(item,index) in contractList" :key="index" class="contract-flex_preview">
+                    <div class="preview_name">合同：</div>
+                    {{item.fileName}}
+                    <a class="preview_btn" :href="item.fileUrl" target='_blank'>预览</a>
+                    <div class="preview_btn" @click="onDelect(index)">删除</div>
                 </div>
 
                 <div class="contract-flex_bot">
-                    <h-button type="primary" @click="onSubmitApprove">提交修订</h-button>
+                    <h-button type="primary" @click="onSubmitApprove" :disabled='contractList.length<1'>提交修订</h-button>
                 </div>
             </div>
             <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-                <span>提交后，即分财(风控/法务)审核通过，确定此操作吗？</span>
-                <span>驳回后，即分财(风控/法务)审核不通过，确定此操作吗？</span>
+                <span v-if="type==1">提交后，即分财(风控/法务)审核通过，确定此操作吗？</span>
+                <span v-else>驳回后，即分财(风控/法务)审核不通过，确定此操作吗？</span>
                 <el-form :model="form" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-                    <el-form-item label="意见" prop="approval_Remark">
-                        <el-input type="textarea" :rows="2" v-model="approval_Remark" maxlength="100"></el-input>
+                    <el-form-item label="意见" prop="approvalRemark" ref="approval_Remark">
+                        <el-input type="textarea" :rows="4" v-model="form.approvalRemark" maxlength="100"></el-input>
                     </el-form-item>
                 </el-form>
                 <span slot="footer" class="dialog-footer">
@@ -57,7 +59,7 @@ export default {
             // 上传时附带的额外参数同el-upload 的 data
             uploadParameters: {
                 updateUid: '',
-                reservedName: false
+                reservedName: true
             },
             contractList: [],
             approval_Remark: '',
@@ -71,40 +73,54 @@ export default {
             },
             dialogVisible: false,
             rules: {
-                approval_Remark: [
-                    { required: true, message: '123123', trigger: 'blur' }
-                ],
-            }
+                approvalRemark: [
+                    { required: true, message: '请输入意见' }
+                ]
+            },
+            type: 1
         }
+    },
+    watch: {
+
     },
     methods: {
         handleSuccessCb (row) {
             console.log(row)
         },
         onResive () {
+            this.type = 1
             this.dialogVisible = true
         },
         handleClose () {
             this.dialogVisible = false
         },
+        onDelect (index) {
+            this.contractList = this.contractList.splice(1, index)
+        },
         onSubmitMsg () {
-
+            // 驳回合同
             const parms = {
                 'contractId': this.$route.query.id,
                 'approver': this.userInfo.employeeName,
                 "approverRole": this.$route.query.role,
-                "approvalRemark": this.approval_Remark,
+                "approvalRemark": this.form.approvalRemark,
             }
+            // rejectContracts(parms)
             this.$refs.ruleForm.validate(async (valid) => {
                 if (valid) {
                     await rejectContracts(this.form)
-
+                    this.$message({
+                        message: '当前合同已被驳回！',
+                        type: 'success'
+                    });
+                    this.$router.push('/goodwork/contractSigningManagement')
                 } else {
 
                 }
             })
         },
         async onSubmitApprove () {
+            this.type = 2
             this.dialogVisible = true
             // this.form.approver = this.userInfo.employeeName
             // this.contractList.map(item => {
@@ -143,6 +159,11 @@ export default {
     background: #f5f5f5;
     flex-direction: column;
     padding: 20px;
+    border-radius: 10px;
+    box-sizing: 10px 10px 5px #efefee;
+    &_tips {
+        color: #f00;
+    }
     p {
         margin-top: 5px;
     }
@@ -152,7 +173,15 @@ export default {
     }
     &_preview {
         display: flex;
-        justify-content: center;
+        margin-top: 10px;
+    }
+    .preview_btn {
+        color: #ff7a45;
+        margin-left: 20px;
+        cursor: pointer;
+    }
+    .preview_name {
+        color: #000;
     }
 }
 </style>
