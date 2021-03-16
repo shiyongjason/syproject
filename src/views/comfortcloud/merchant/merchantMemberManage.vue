875d91c99930cf68e05a5a9716087d4a6d03facc
yangliu <liu.yang@hosjoy.com>
2021-03-16 16:50:46 +0800

    iot-12790 修改会员标签文案错误问题

--- src/views/comfortcloud/merchant/merchantMemberManage.vue
+++ src/views/comfortcloud/merchant/merchantMemberManage.vue
@@ -121,6 +121,7 @@
                     </div>
                 </div>
                 <span slot="footer" class="dialog-footer">
+                     <el-button @click="addNewTag()">新增标签</el-button>
                     <el-button @click="tagCancelSelect()">清除已选中的标签</el-button>
                     <el-button @click="tagCancel()">取消</el-button>
                     <el-button type="primary" @click="editConform()">确认</el-button>
@@ -366,6 +367,10 @@ export default {
                 // 不存在则添加
                 datas.push(tag)
             }
+        },
+        addNewTag () {
+            this.clearData()
+            this.$router.push({ path: '/comfortCloudMerchant/merchantVIP/merchantMemberTag' })
         },
         onEdit (val) {
             this.$router.push({ path: '/comfortCloudMerchant/merchantVIP/merchantMemberInvitation', query: val })
