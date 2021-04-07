f4b1389b6b0b072c813eee76345ba57ebea4ffb0
Mac <haojieguangxin@163.com>
2021-04-07 18:24:02 +0800

    HAM-25281  【前端】资金支持余额汇总表增加截止日期查询

--- src/views/wisdom/fundsData/Ledger.vue
+++ src/views/wisdom/fundsData/Ledger.vue
@@ -110,6 +110,13 @@
                     </el-date-picker>
                 </div>
             </div>
+            <div class="query-cont-col" v-if="accountType == 0">
+                <div class="query-col-title">截止日期：</div>
+                <div class="query-col-input">
+                    <el-date-picker type="date" :editable="false" :clearable="true" v-model="queryParams.queryEndDate" value-format="yyyy-MM-dd" placeholder="截止日期">
+                    </el-date-picker>
+                </div>
+            </div>
             <div class="query-cont-col">
                 <div class="query-col-title">
                     <el-button type="primary" class="ml20" @click="onSearch">查询</el-button>
@@ -206,7 +213,8 @@ export default {
                 repaymentType: '',
                 startDate: '',
                 endDate: '',
-                repaymentStatus: ''
+                repaymentStatus: '',
+                queryEndDate: ''
             },
             searchParams: {},
             removeValue: false,
@@ -434,6 +442,7 @@ export default {
             this.$set(this.queryParams, 'startDate', '')
             this.$set(this.queryParams, 'endDate', '')
             this.$set(this.queryParams, 'repaymentStatus', '')
+            this.$set(this.queryParams, 'queryEndDate', '')
             this.selectAuth.regionObj = { ...obj }
             this.selectAuth.branchObj = { ...obj }
             this.selectAuth.platformObj = { ...obj }
