cf07037b99753c818bbd1ef87c4746935f4c6b27
weiyou789 <weiyou789567@163.com>
2021-05-13 16:25:55 +0800

    修复编辑和复制库存不显示问题

--- src/views/hmall/marketManage/seckillManage/createSeckill.vue
+++ src/views/hmall/marketManage/seckillManage/createSeckill.vue
@@ -266,7 +266,7 @@ export default {
                     render: (h, scope) => {
                         return (
                             <el-form-item prop={`spikeSku[${scope.$index}].availableStock`} label={scope.row.cityName} rules={this.availableStockRules} label-width="10px" style="margin-top: 20px">
-                                <el-input value={scope.row.inventoryRemainNum} style='width:50%' size='mini' onInput={(val) => { this.setOneCol(Number(val.replace(/[^\d]/g, '')), scope, 'availableStock') }} disabled={this.disabled}></el-input>
+                                <el-input value={scope.row.availableStock} style='width:50%' size='mini' onInput={(val) => { this.setOneCol(Number(val.replace(/[^\d]/g, '')), scope, 'availableStock') }} disabled={this.disabled}></el-input>
                             </el-form-item>
                         )
                     }
@@ -660,7 +660,7 @@ export default {
             this.form.spikeSku.forEach(item => {
                 !item.purchaseLimitNum && this.$set(item, 'purchaseLimitNum', '') // 限购数量
                 !item.sellingPoint && this.$set(item, 'sellingPoint', '') // 卖点信息
-                item.availableStock && this.$set(item, 'inventoryRemainNum', item.availableStock)
+                item.availableStock ? this.$set(item, 'inventoryRemainNum', item.availableStock) : this.$set(item, 'availableStock', item.inventoryRemainNum)
                 // 为什么优惠信息这块的判断比较特别，是因为优惠信息可以为0，限购数量和库存都不可以为0
                 if (!item.discountValue && item.discountValue != 0) this.$set(item, 'discountValue', '') // 优惠信息
                 !item.productId && this.$set(item, 'productId', null) // 产品ID
