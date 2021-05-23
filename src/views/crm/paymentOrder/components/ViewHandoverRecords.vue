174c9939490868c8a31f23977ecfa001367150e5
sunjun <yomingyo@gmail.com>
2021-05-23 16:03:49 +0800

    HAM-27054 【前端】BOSS支付单-发起放款交接单

--- src/views/crm/paymentOrder/components/ViewHandoverRecords.vue
+++ src/views/crm/paymentOrder/components/ViewHandoverRecords.vue
@@ -2,7 +2,7 @@
     <div class="tab-layout" >
         <div v-for="(item,index) in loanTransferRecord" :key="index + 'loanTransferRecord'">
             <div class="info-layout">
-                <div class="info-layout-item"><font style="color:#169BD5;margin-right:6px;min-width: 32px;">{{item.operator||'-'}}</font><span>{{item.operator||'-'}}</span></div>
+                <div class="info-layout-item"><font style="color:#169BD5;margin-right:6px;min-width: 32px;">{{item.operator||'-'}}</font><span>{{item.operationContent||'-'}}</span></div>
                 <div class="info-layout-item" style="flex:0 0 150px"><font>{{item.operatorTime|formatterTime}}</font></div>
             </div>
             <div class="remark-infos">
