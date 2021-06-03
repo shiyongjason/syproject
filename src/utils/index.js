59bb2fd384ef682da1de611058f3766cdfe14524
sunjun <yomingyo@gmail.com>
2021-06-03 11:07:57 +0800

    HAM-27933 【前端】放款交接单下载方式 ，下载放款交接接口调整

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 import store from '@/store/index'
+import axios from 'axios'
 
 const clearCache = function (routerName) {
     store.commit('REMOVE_CACHED_INCLUDE', routerName)
@@ -9,6 +10,31 @@ const newCache = function (routerName) {
     store.commit('REMOVE_CACHED_EXCLUDE', routerName)
 }
 
+const downloadFile = function (apiUrl) {
+    // let apiUrl = `project/api/loan-transfers/boss/${this.paymentOrderId}/download`
+    // window.open(apiUrl, '_blank')
+    axios.get(apiUrl, {
+        responseType: 'blob' // 指定获取数据的类型为blob
+    }).then(res => {
+        let fileName = ''
+        let disposition = res.headers['content-disposition']
+        if (disposition && disposition.indexOf('attachment') !== -1) {
+            fileName = disposition.match(/filename=(.*)/)[1]
+            let blobURL = window.URL.createObjectURL(new Blob([res.data]))
+            let link = document.createElement('a')
+            link.style.display = 'none'
+            link.href = blobURL
+            link.setAttribute('download', decodeURIComponent(fileName))
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link) // 下载完成移除元素
+            window.URL.revokeObjectURL(blobURL) // 释放blob对象
+        } else {
+            console.error(' 让后端response的header里面设置Access-Control-Expose-Headers: Content-Disposition， 参考/goodwork/upstreamPaymentManagement onLoanDown的接口')
+        }
+    })
+}
+
 export {
-    clearCache, newCache
+    clearCache, newCache, downloadFile
 }
