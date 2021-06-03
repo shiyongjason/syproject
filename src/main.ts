8318d23b4f2e2429303169a31c860b4396525e6a
shiyong <422703012@qq.com>
2021-06-03 11:45:11 +0800

    sentry

--- src/main.ts
+++ src/main.ts
@@ -39,16 +39,16 @@ import precisionMethods from '@/utils/bignumber.js'
 
 // 接入 sentry
 // @ts-ignore
-import * as Sentry from '@sentry/browser'
-import { Vue as VueIntegration } from '@sentry/integrations'
-if (process.env.NODE_ENV === 'production' || process.env.VUE_APP_TITLE === 'preview') {
-    Sentry.init({
-        dsn: 'https://96d38d85c5da49bfacdada98edca3ea7@sentry.hosjoy.com/4',
-        integrations: [new VueIntegration({ Vue, attachProps: true, logErrors: true })],
-        release: process.env.RELEASE_VERSION,
-        environment: process.env.VUE_APP_TITLE || process.env.NODE_ENV
-    })
-}
+// import * as Sentry from '@sentry/browser'
+// import { Vue as VueIntegration } from '@sentry/integrations'
+// if (process.env.NODE_ENV === 'production') {
+//     Sentry.init({
+//         dsn: 'https://96d38d85c5da49bfacdada98edca3ea7@sentry.hosjoy.com/4',
+//         integrations: [new VueIntegration({ Vue, attachProps: true, logErrors: true })],
+//         release: process.env.RELEASE_VERSION,
+//         environment: process.env.VUE_APP_TITLE || process.env.NODE_ENV
+//     })
+// }
 
 Vue.config.productionTip = false
 moment.locale('zh-cn')
