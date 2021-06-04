815d51bf244517762095bb63f179c3eb7193bbed
lyn <1571364303@qq.com>
2021-06-04 11:16:33 +0800

    BTB-28788 【前端】【BOSS】运费订单详情页面开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,35 +7,35 @@ module.exports = {
         parser: '@typescript-eslint/parser'
     },
     plugins: [
-        '@typescript-eslint',
+        '@typescript-eslint'
     ],
     extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        "indent": ["error", 4, {
-            "SwitchCase": 1,
-            "VariableDeclarator": 1,
-            "outerIIFEBody": 1,
-            "MemberExpression": 1,
-            "FunctionDeclaration": {
-                "parameters": 1,
-                "body": 1
+        'indent': ['error', 4, {
+            'SwitchCase': 1,
+            'VariableDeclarator': 1,
+            'outerIIFEBody': 1,
+            'MemberExpression': 1,
+            'FunctionDeclaration': {
+                'parameters': 1,
+                'body': 1
             },
-            "FunctionExpression": {
-                "parameters": 1,
-                "body": 1
+            'FunctionExpression': {
+                'parameters': 1,
+                'body': 1
             },
-            "CallExpression": {
-                "arguments": 1
+            'CallExpression': {
+                'arguments': 1
             },
-            "ArrayExpression": 1,
-            "ObjectExpression": 1,
-            "ImportDeclaration": 1,
-            "flatTernaryExpressions": false,
-            "ignoreComments": false
+            'ArrayExpression': 1,
+            'ObjectExpression': 1,
+            'ImportDeclaration': 1,
+            'flatTernaryExpressions': false,
+            'ignoreComments': false
         }],
         'eol-last': 'off',
-        "eqeqeq": 0
+        'eqeqeq': 0
     }
 }
