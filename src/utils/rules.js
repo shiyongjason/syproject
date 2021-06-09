fdc605ab69aa4063ca22b46ca63a9cd02d9362d7
lyn <1571364303@qq.com>
2021-06-09 10:46:39 +0800

    BTB-29300 【boss】运费价格维护输入998.99提示仅支持输入999内两位小数正数（最小0.01）

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -223,9 +223,14 @@ export const checkNumandEng = (rule, value, callback) => {
 
 // 999内两位小数正数（最小0.01）
 export const withinDetail = (rule, value, callback) => {
-    const Reg = /^([1-9]\d{0,2})$|^0[.]([1-9]\d{0,1}|[0-9][1-9])$/
-    if (value && !(Reg.test(value))) {
+    const Reg = /^0\.([1-9]|\d[1-9])$|^[1-9]\d{0,2}\.\d{0,2}$|^[1-9]\d{0,2}$/
+    if (value <= 999) {
+        if (value && !(Reg.test(value))) {
+            return callback(new Error('仅支持输入999内两位小数正数（最小0.01）'))
+        }
+    } else {
         return callback(new Error('仅支持输入999内两位小数正数（最小0.01）'))
     }
+
     callback()
 }
