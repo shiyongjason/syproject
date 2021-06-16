af67cd96d6271728fe26f49c8b8def861c976b08
sunjun <yomingyo@gmail.com>
2021-06-16 18:07:39 +0800

    IOT-15100 IoT 线索一期--前端--boss 2.0项目列表

---
+++ src/views/crm/projectList2_0/const/index.ts
@@ -0,0 +1,67 @@
+/** 支付方式 */
+export const PAYMENTMETHOD: { label: string; value: number; }[] = [
+    { label: '对公打款', value: 1 },
+    { label: '银行转账', value: 2 },
+    { label: '支付宝转账', value: 3 },
+    { label: '微信转账', value: 4 },
+    { label: '现金支付', value: 5 },
+    { label: '其他', value: 6 }
+]
+/** 销售阶段 */
+export const SALESPHASE: { label: string; value: number; }[] = [
+    { label: '首次接触', value: 1 },
+    { label: '需求确认', value: 2 },
+    { label: '提供方案/报价', value: 3 },
+    { label: '商务洽谈，待签约', value: 4 },
+    { label: '已签约', value: 5 },
+    { label: '已回款', value: 6 },
+    { label: '无需跟进', value: 7 }
+]
+/** 建筑类型 */
+export const BUILDINGTYPE: { label: string; value: number; }[] = [
+    { label: '医院', value: 1 },
+    { label: '酒店', value: 2 },
+    { label: '学校', value: 3 },
+    { label: '政府公建项目', value: 4 },
+    { label: '商用写字楼', value: 5 },
+    { label: '商场购物中心', value: 6 },
+    { label: '地产项目', value: 7 },
+    { label: '厂房（含养殖业）', value: 8 },
+    { label: '其他', value: 9 }
+]
+/** 项目角色 */
+export const PROJECTROLE: { label: string; value: number; }[] = [
+    { label: '设备供应商', value: 1 },
+    { label: '辅材供应商', value: 2 },
+    { label: '安装服务商', value: 3 },
+    { label: '系统集成商', value: 4 },
+    { label: '总包', value: 5 },
+    { label: '分包', value: 6 },
+    { label: '其他', value: 7 }
+]
+/** 所处的阶段 */
+export const ATTHESTAGE: { label: string; value: number; }[] = [
+    { label: '采购阶段——辅材未定', value: 1 },
+    { label: '采购阶段——辅材已定', value: 2 },
+    { label: '项目跟踪阶段——方案未定', value: 3 },
+    { label: '项目跟踪阶段——方案已定', value: 4 },
+    { label: '招投标阶段', value: 5 },
+    { label: '无在谈/在投项目', value: 6 }
+]
+/** 可从总部采购产品 */
+export const PURCHASEPRODUCTS: { label: string; value: number; }[] = [
+    { label: '冷暖辅材', value: 1 },
+    { label: '电线电缆', value: 2 },
+    { label: '管材管件', value: 3 },
+    { label: '热水设备', value: 4 },
+    { label: '高端厨电', value: 5 },
+    { label: '智能化产品', value: 6 }
+]
+/** 工程项目智能化需求 */
+export const INTELLIGENTDEMAND: { label: string; value: number; }[] = [
+    { label: '需集控方案', value: 1 },
+    { label: '需能源监测功能', value: 2 },
+    { label: '需节能计费功能', value: 3 },
+    { label: '需智控面板', value: 4 },
+    { label: '其他', value: 5 }
+]
---
+++ src/views/crm/projectList2_0/detail.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-drawer title="项目详情" :visible.sync="drawer" :before-close="handleClose" :modal-append-to-body='false' size='680px'>
+        <div class="ProjectList2Detail">
+            <el-radio-group v-model="radio1">
+                <el-radio-button label="跟进记录"></el-radio-button>
+                <el-radio-button label="项目信息"></el-radio-button>
+            </el-radio-group>
+            <div class="ProjectList2Detail-ctx">
+                <div>
+                    <b>项目类型：</b>项目植入供应链产品
+                </div>
+                <!-- 流程 -->
+                <div class="process">
+                    <div class="process-item isActive">首次接触</div>
+                    <div class="step_arrow"></div>
+                    <div class="process-item isActive">需求确认</div>
+                    <div class="step_arrow"></div>
+                    <div class="process-item isActive">提供方案/报价</div>
+                    <div class="step_arrow"></div>
+                    <div class="process-item isActive">商务洽谈，待签约</div>
+                    <div class="step_arrow down"></div>
+                    <div class="process-item pos5">商务洽谈，待签约</div>
+                    <div class="step_arrow left"></div>
+                    <div class="process-item pos6">已签约</div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </el-drawer>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+
+@Component({
+    name: 'ProjectList2Detail',
+    components: {
+    }
+})
+export default class ProjectList2Detail extends Vue {
+    @Prop({ type: Boolean, required: true, default: false }) drawer:any
+    radio1: string = '跟进记录';
+
+    handleClose () {
+        this.$emit('backEvent')
+    }
+}
+</script>
+<style lang='scss' scoped>
+@import "./css/detail.scss";
+</style>
