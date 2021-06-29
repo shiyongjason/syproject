e7856ac9ca09465d9b18071e593c59e53c7df1ff
shiyong <422703012@qq.com>
2021-06-29 09:28:52 +0800

    工程方案

--- src/views/crm/merchanEnginePlan/api/index.js
+++ src/views/crm/merchanEnginePlan/api/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
-const iotUrl = 'https://testiot.hosjoy.com:2286/'
 // 工程方案列表
-export const getCrmPlanList = (params) => axios.get(iotUrl + '/mall/boss/project-scheme', { params })
+export const getCrmPlanList = (params) => axios.get('/memeber/api/boss/project-scheme', { params })
 // 获取工程方案详情
-export const getCrmPlanDetail = params => axios.get(iotUrl + '/mall/boss/project-scheme/' + params.id)
+export const getCrmPlanDetail = params => axios.get('/memeber/api/boss/project-scheme' + params.id)
 // 新增修改工程方案
-export const addCrmPlanDetail = params => axios.post(iotUrl + '/mall/boss/project-scheme', params)
+export const addCrmPlanDetail = params => axios.post('/memeber/api/boss/project-scheme', params)
 // 删除工程方案
-export const deleteProjectScheme = params => axios.delete(iotUrl + '/mall/boss/project-scheme/' + params.id)
+export const deleteProjectScheme = params => axios.delete('/memeber/api/boss/project-scheme' + params.id)
