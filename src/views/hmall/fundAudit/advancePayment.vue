c31bb530397e73f425a090b0f98d20accfd74df0
lyn <1571364303@qq.com>
2021-07-13 16:29:42 +0800

    BTB-30695 【boss】提前还款确认列表页，提交时间和确认时间应该展示到秒

--- src/views/hmall/fundAudit/advancePayment.vue
+++ src/views/hmall/fundAudit/advancePayment.vue
@@ -156,7 +156,7 @@ export default {
             tableLabel: [
                 { label: '代采订单号', prop: 'agentOrderNo' },
                 { label: 'mis订单号', prop: 'misOrderNo' },
-                { label: '提交时间', prop: 'createTime', formatters: 'dateTimes' },
+                { label: '提交时间', prop: 'createTime', formatters: 'dateTime' },
                 { label: '代采订单总金额', prop: 'totalAmount' },
                 { label: '代采商品数量', prop: 'quantity' },
                 { label: '预付款', prop: 'prepayAmount' },
@@ -166,7 +166,7 @@ export default {
                 { label: '预付款状态', prop: 'prepayStatus' },
                 { label: '打款账号', prop: 'prepayBankCardNo' },
                 { label: '打款账户名', prop: 'prepayAccountName' },
-                { label: '确认时间', prop: 'prepayConfirmTime', formatters: 'dateTimes' },
+                { label: '确认时间', prop: 'prepayConfirmTime', formatters: 'dateTime' },
                 { label: '订单状态', prop: 'orderSwitch' },
                 { label: '订单同步状态', prop: 'orderSyncStatus' },
                 { label: '资金同步状态', prop: 'fundSyncStatus' },
