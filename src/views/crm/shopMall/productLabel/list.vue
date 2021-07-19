246d8e0eb67f375bb144e94ac866aed6c7e7f4d0
sunjun <yomingyo@gmail.com>
2021-07-19 14:44:15 +0800

    fix maxSalePrice

--- src/views/crm/shopMall/productLabel/list.vue
+++ src/views/crm/shopMall/productLabel/list.vue
@@ -151,7 +151,7 @@ export default class ProductLabel extends Vue {
                 return (
                     <div>
                         {!scope.row.priceVisible ? '不展示'
-                            : scope.row.minSalePrice === null && scope.row.minSalePrice === null ? '-' : <span>{utils.money(scope.row.minSalePrice)}-{utils.money(scope.row.maxSalePrice)}</span>
+                            : scope.row.minSalePrice === null && scope.row.maxSalePrice === null ? '-' : <span>{utils.money(scope.row.minSalePrice)}-{utils.money(scope.row.maxSalePrice)}</span>
                         }
                     </div>
                 )
