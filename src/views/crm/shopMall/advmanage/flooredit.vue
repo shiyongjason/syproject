4fd1b47efe7f7d7a2055e9b7bee1ace2e460cd6a
sunjun <990375133@qq.com>
2021-07-23 09:29:18 +0800

    add

--- src/views/crm/shopMall/advmanage/flooredit.vue
+++ src/views/crm/shopMall/advmanage/flooredit.vue
@@ -35,13 +35,13 @@
                 </div>
             </div>
             <div class="mb20">
-                <h-button type="primary" @click="onBatch">批量选择</h-button>
+                <h-button type="primary" @click="onBatch" :disabled=isDisabled>批量选择</h-button>
             </div>
-            <hosJoyTable ref="multipleTable" align="center" isShowIndex border showPagination :column="tableLabel" :data="tableData" :pageNumber.sync="queryParams.pageNumber" :pageSize.sync="queryParams.pageSize" :total="page.total" @pagination="onFindList" :tableRowClassName="rowClass"
+            <hosJoyTable ref="multipleTable" v-if="tableData" align="center" isShowIndex border showPagination :column="tableLabel" :data="tableData" :pageNumber.sync="queryParams.pageNumber" :pageSize.sync="queryParams.pageSize" :total="page.total" @pagination="onFindList" :tableRowClassName="rowClass"
                 :selectable="selectable" :pageSizes='page.sizes' actionWidth='100' isAction :max-height="500" @selection-change="handleSelectionChange" isShowselection :isActionFixed='tableData&&tableData.length>0'>
                 <template #action="slotProps">
-                    <h-button table v-if="!slotProps.data.row.checked" @click="onSelect(slotProps.data)">选择</h-button>
-                    <h-button table v-if="slotProps.data.row.checked" @click="onNoSelect(slotProps.data)">取消选择</h-button>
+                    <el-button  size="mini" type="primary" v-if="!slotProps.data.row.checked" @click="onSelect(slotProps.data)">选择</el-button>
+                    <el-button  size="mini"  v-if="slotProps.data.row.checked" @click="onNoSelect(slotProps.data)">取消选择</el-button>
                 </template>
             </hosJoyTable>
             <div class="floor-tit mt20">已选择该楼层的商品</div>
@@ -96,6 +96,7 @@ export default class Flooredit extends Vue {
         reservedName: false
     }
     show: boolean = false
+    isDisabled: boolean = true
     _category=[]
     selectRow = []
     selectData = []
@@ -195,7 +196,7 @@ export default class Flooredit extends Vue {
 
     handleSelectionChange (val) {
         this.selectData = val
-        console.log(val)
+        this.isDisabled = !(val.length > 0)
     }
     onBatch () {
         if (this.selectData.length > 0) {
@@ -212,6 +213,8 @@ export default class Flooredit extends Vue {
                 }
                 this.$set(item, 'checked', true)
             })
+        } else {
+            this.$message.warning('请选择商品')
         }
     }
     // 选中行 换色
@@ -228,19 +231,19 @@ export default class Flooredit extends Vue {
 
     async onFindList () {
         const { data: spu } = await getSpuPage(this.queryParams)
-        this.tableData = spu.records
         this.page.total = spu.total as number
         // 查询时候 查下最新的是否选中状态
         if (this.tableForm.length > 0) {
-            this.tableData.length > 0 && this.tableData.map((item, index) => {
-                this.tableForm.map((jtem, index) => {
+            this.tableForm.map((jtem, jndex) => {
+                spu.records.length > 0 && spu.records.map((item:any, index) => {
                     if (jtem.id == item.id) {
-                        console.log('car')
                         item.checked = true
                     }
                 })
             })
         }
+        this.tableData = [...spu.records]
+        console.log(this.tableData)
     }
 
     async onFindCateList () {
@@ -250,13 +253,14 @@ export default class Flooredit extends Vue {
     }
 
     onSelect (val) {
-        // val.row.checked = true
         this.$set(this.tableData[val.$index], 'checked', true)
         this.tableForm.push(val.row)
         this.$refs['multipleTable'].toggleRowSelection(val.row)
+        this.$forceUpdate()
     }
     onNoSelect (val) {
-        this.$set(val.row, 'checked', false)
+        this.$set(this.tableData[val.$index], 'checked', false)
+        // this.$set(val.row, 'checked', false)
         let _arr = this.tableForm.filter(i => val.row.id == i.id)
         if (_arr.length > 0) {
             let one = this.tableForm.findIndex(value => {
@@ -294,6 +298,7 @@ export default class Flooredit extends Vue {
             let one = this.tableData.findIndex(value => {
                 return value.id == _arr[0].id
             })
+            console.log('one', one)
             this.$set(this.tableData[one], 'checked', false)
         }
         this.tableForm.splice(val.$index, 1)
