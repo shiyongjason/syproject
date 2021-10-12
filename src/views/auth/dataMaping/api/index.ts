f9a7185e2b444a566b7a4c6864395d61e3a536df
shiyong <422703012@qq.com>
2021-10-12 15:22:25 +0800

    :zap: 数据改造

--- src/views/auth/dataMaping/api/index.ts
+++ src/views/auth/dataMaping/api/index.ts
@@ -2,14 +2,26 @@
 import { IPagePrepaymentResponse } from '@/interface/hbp-project'
 import axios, { AxiosPromise } from 'axios'
 
-export const findOrganizationTree: () => AxiosPromise<any> = () => {
-    return axios.get('/uaa/api/organization/tree', { })
+export const findOrganizationTree: (type) => AxiosPromise<any> = (type) => {
+    return axios.get(`/uaa/api/organization/tree/${type}`, { })
 }
 
 export const findEhrTree: () => AxiosPromise<any> = () => {
     return axios.get('/uaa/department/getDepartmentTree', { })
 }
 
-export const updateOrganization: (params) => AxiosPromise<any> = (params) => {
-    return axios.get('/uaa/api/organization', { params })
+export const updateOrganizationTree: (params) => AxiosPromise<any> = (params) => {
+    return axios.post('/uaa/api/organization/tree', params)
+}
+
+export const updateOrganizationNode: (params) => AxiosPromise<any> = (params) => {
+    return axios.post('/uaa/api/organization', params)
+}
+
+export const editOrganizationNode: (params) => AxiosPromise<any> = (params) => {
+    return axios.put('/uaa/api/organization', params)
+}
+
+export const deleteOrganizationNode: (id) => AxiosPromise<any> = (id) => {
+    return axios.delete(`/uaa/api/organization/${id}`)
 }
--- src/views/auth/dataMaping/index.vue
+++ src/views/auth/dataMaping/index.vue
@@ -8,7 +8,7 @@
                     </div>
                     <el-input placeholder="输入关键字进行过滤" v-model="filterEhr">
                     </el-input>
-                    <el-tree ref="leftTree" :data="data1" show-checkbox :expand-on-click-node="false" :check-strictly='true' node-key="id" default-expand-all :props="{label:'deptname',children:'childNodeList'}" :filter-node-method="filterEhrNode">
+                    <el-tree ref="leftTree" :data="ehrList" show-checkbox :expand-on-click-node="false" :check-strictly='true' node-key="id" default-expand-all :props="{label:'deptname',children:'children'}" :filter-node-method="filterEhrNode">
                     </el-tree>
                 </div>
                 <div class="page-center">
@@ -22,17 +22,18 @@
                     </div>
                     <el-input placeholder="输入关键字进行过滤" v-model="filterBoss">
                     </el-input>
-                    <el-tree ref="rightTree" :data="data" node-key="id" show-checkbox :expand-on-click-node="false" default-expand-all :check-strictly='true' @check-change="handleCheckChange" draggable :props="{label:'deptname',children:'childNodeList'}" @node-drag-end="handleDragEnd" :allow-drop="allowDrop" :filter-node-method="filterBossNode">
+                    <el-tree ref="rightTree" :data="bossList" node-key="id" show-checkbox :expand-on-click-node="false" default-expand-all :check-strictly='true' @check-change="handleCheckChange" draggable :props="{label:'organizationName',children:'childOrganizations'}"
+                        @node-drag-end="handleDragEnd" :allow-drop="allowDrop" :filter-node-method="filterBossNode">
                         <span class="custom-tree-node" slot-scope="{ node, data }">
                             <span>{{ node.label }} </span>
                             <span class="page-right-tree">
-                                <template v-for="(item,index) in data.ehrNode">
-                                    <el-tag closable @close='handleCloseTag(data.ehrNode,item,index,node)' size="mini" type="info" :key=item.id>{{item.label}}</el-tag>
+                                <template v-for="(item,index) in data.ehrDeptResponseList">
+                                    <el-tag closable @close='handleCloseTag(data.ehrDeptResponseList,item,index,node)' size="mini" type="info" :key=item.id>{{item.ehrDeptName}}</el-tag>
                                 </template>
-                                <el-button type="text" size="mini" @click="() => edit(data)">
+                                <el-button type="text" size="mini" @click="() => edit(data,'edit')">
                                     编辑
                                 </el-button>
-                                <el-button type="text" size="mini" @click="() => append(data)">
+                                <el-button type="text" size="mini" @click="() => append(data,'add')">
                                     新增
                                 </el-button>
                                 <el-button type="text" size="mini" @click="() => remove(node, data)">
@@ -43,11 +44,11 @@
                     </el-tree>
                 </div>
             </div>
-             <el-button type="primary" @click="handleSumbit">确定</el-button>
+            <el-button type="primary" @click="handleSumbit">确 定</el-button>
         </div>
-        <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-            <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="名称">
+        <el-dialog title="节点信息" :visible.sync="dialogVisible" :close-on-click-modal=false width="30%" :before-close="handleClose">
+            <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+                <el-form-item label="节点名称" prop="name">
                     <el-input v-model="form.name"></el-input>
                 </el-form-item>
             </el-form>
@@ -61,8 +62,8 @@
 
 <script lang='tsx'>
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
-import { State } from 'vuex-class'
-import { findOrganizationTree, findEhrTree } from './api/index'
+import { State, Action } from 'vuex-class'
+import { findOrganizationTree, findEhrTree, updateOrganizationTree, updateOrganizationNode, editOrganizationNode, deleteOrganizationNode } from './api/index'
 import './index.scss'
 
 @Component({
@@ -74,19 +75,17 @@ export default class Datamaping extends Vue {
         rightTree: HTMLFormElement,
         leftTree:HTMLFormElement
     }
-    id: number = 1000;
+    id: number = 1010
     ehrData: Record<string, string> = {}
     bossData: Record<string, string> = {}
     filterEhr:string = ''
     filterBoss:string = ''
     dialogVisible:boolean = false
-    form = {}
-    data1: Array<any> = []
-    data: Array<any> = []
-    defaultProps = {
-        children: 'children',
-        label: 'label'
-    }
+    form:any = {}
+    pNode:any = {}
+    type:string = ''
+    ehrList: Array<any> = []
+    bossList: Array<any> = []
 
     @Watch('filterEhr') findEhr (val) {
         this.$refs.leftTree.filter(val)
@@ -102,42 +101,53 @@ export default class Datamaping extends Vue {
     }
     filterBossNode (value, data) {
         if (!value) return true
-        return data.label.indexOf(value) !== -1
+        return data.organizationName.indexOf(value) !== -1
     }
+    rules = {
+        name: [
+            { required: true, message: '名称必填', trigger: blur }
+        ]
+    }
+
     onMatching () {
         const bossData = this.onClickBoss()
         const ehrData = this.onClickEHR()
         if (ehrData.length == 0) {
-            this.$message.warning('EHR组织机构')
+            this.$message.warning('请选择EHR组织机构节点')
             return
         }
         if (bossData.length == 0) {
-            this.$message.warning('boss组织机构')
+            this.$message.warning('请选择Boss组织机构节点')
             return
         }
         // console.log(this.checkMaping(this.bossData, this.data, this.ehrData))
         ehrData.map(item => {
-            this.data = this.checkMaping(bossData[0], this.data, item)
-            this.checkMaping(item, this.data1, '')
+            this.bossList = this.checkMaping(bossData[0], this.bossList, item)
+            this.checkMaping(item, this.ehrList, '')
         })
         this.$refs.rightTree.setCheckedKeys([])
     }
 
     // 递归 匹配  boss 数据  ehr数据
-    checkMaping (val, data, ehr) {
+    checkMaping (val, data, ehr:any) {
         return data.map((item: any) => {
             if (item.id == val.id) {
+                console.log('==', item.id, val.id)
                 //
                 if (ehr == '') {
+                    console.log('go')
                     item.disabled = true
-                    // this.$refs.leftTree.setCheckedKeys([val.id], false)
                     this.$refs.leftTree.setCheckedKeys([])
                 } else {
-                    if (item.ehrNode) {
-                        item.ehrNode.push(ehr)
+                    ehr = {
+                        ehrDeptCode: ehr.deptcode,
+                        ehrDeptName: ehr.deptname
+                    }
+                    if (item.ehrDeptResponseList) {
+                        item.ehrDeptResponseList.push(ehr)
                     } else {
-                        item.ehrNode = []
-                        item.ehrNode.push(ehr)
+                        item.ehrDeptResponseList = []
+                        item.ehrDeptResponseList.push(ehr)
                     }
                 }
                 // this.$set(this.data, item, ehr)
@@ -145,51 +155,69 @@ export default class Datamaping extends Vue {
             if (item.children) {
                 item.children = this.checkMaping(val, item.children, ehr)
             }
+            if (item.childOrganizations) {
+                item.childOrganizations = this.checkMaping(val, item.childOrganizations, ehr)
+            }
             return item
         })
     }
-    // 重组数据 末级有 父级不可以选
-    onCheckLastStage (data) {
+    // 重组EHR数据 末级有 父级不可以选
+    onCheckEhrLastStage (data) {
         return data.map(item => {
-            // item.ehrNode = ''
+            item.children = item.childNodeList
+            item.id = item.deptcode
             if (item.childNodeList && item.childNodeList.length > 0) {
                 item.disabled = true
-                item.childNodeList = this.onCheckLastStage(item.childNodeList)
+                item.childNodeList = this.onCheckEhrLastStage(item.childNodeList)
+            }
+            return item
+        })
+    }
+    // 重组BOSS数据 末级有 父级不可以选
+    onCheckBossLastStage (data) {
+        return data.map(item => {
+            // item.children = item.childOrganizations
+            if (item.childOrganizations && item.childOrganizations.length > 0) {
+                item.disabled = true
+                item.childOrganizations = this.onCheckBossLastStage(item.childOrganizations)
             }
             return item
         })
     }
     onCheckEhr (data, val, index) {
         return data.map(item => {
-            if (item.id == val.id) {
+            if (item.id == val.ehrDeptCode) {
                 item.disabled = false
-                if (item.ehrNode) {
-                    console.log(1, index)
+            }
+            if (item.id == val.id) {
+                if (item.ehrDeptResponseList) {
                     // item.ehrNode.filter(i => i.id != val.id)
-                    item.ehrNode.splice(index, 1)
+                    item.ehrDeptResponseList.splice(index, 1)
                 }
             }
             if (item.children) {
                 item.children = this.onCheckEhr(item.children, val, index)
             }
+            if (item.childOrganizations) {
+                item.childOrganizations = this.onCheckEhr(item.childOrganizations, val, index)
+            }
             return item
         })
     }
-    // 放弃当个映射
+    // 放弃单个映射
     handleCloseTag (data, val, index, node) {
         console.log(123, val)
         this.$nextTick(() => {
             // node.data.ehrNode.splice(index, 1)
-            this.data = this.onCheckEhr(this.data, node.data, index)
+            this.bossList = this.onCheckEhr(this.bossList, node.data, index)
             // this.$set(data, index, '')
             // 重新渲染ehr 数据
-            this.data1 = this.onCheckEhr(this.data1, val, index)
+            this.ehrList = this.onCheckEhr(this.ehrList, val, index)
             // this.$forceUpdate()
         })
     }
     // tree 单选
     handleCheckChange (data, checked) {
-        // console.log('00', data)
         if (checked) {
             this.$refs.rightTree.setCheckedKeys([data.id])
         }
@@ -204,24 +232,25 @@ export default class Datamaping extends Vue {
     }
     // 编辑节点
     edit (data) {
-        // 弹窗
         this.dialogVisible = true
+        this.type = 'edit'
+        this.pNode = data
     }
     // 新增节点
     append (data) {
-        console.log('新增', data)
-        const newChild = { id: this.id++, deptname: 'testtest', childNodeList: [] }
-        if (!data.childNodeList) {
-            this.$set(data, 'childNodeList', [])
-        }
-        data.childNodeList.push(newChild)
+        this.dialogVisible = true
+        this.pNode = data
+        this.type = 'add'
     }
-
-    remove (node, data) {
+    // 删除节点
+    async remove (node, data) {
+        console.log(node, data)
         const parent = node.parent
-        const children = parent.data.children || parent.data
-        const index = children.findIndex((d) => d.id === data.id)
-        children.splice(index, 1)
+        const childOrganizations = parent.data.childOrganizations || parent.data
+        const index = childOrganizations.findIndex((d) => d.id === data.id)
+        childOrganizations.splice(index, 1)
+        await deleteOrganizationNode(data.id)
+        this.findBossTree()
     }
 
     handleDragEnd (draggingNode, dropNode, dropType, ev) {
@@ -239,24 +268,53 @@ export default class Datamaping extends Vue {
 
     handleClose () {
         this.dialogVisible = false
+        this.form.name = ''
     }
 
-    handleSumbit () {
-        console.log(this.data)
+    async handleSumbit () {
+        await updateOrganizationTree({ organizationNodeList: this.bossList, createBy: this.userInfo.employeeName })
+        this.findBossTree()
     }
 
     handleSaveNode () {
-
+        let params:any = {
+            organizationName: this.form.name,
+            type: 1
+        }
+        this.$refs['form'].validate(valid => {
+            if (valid) {
+                if (this.type == 'add') {
+                    const newChild = { id: this.id++, organizationName: this.form.name, childOrganizations: [] }
+                    // 展示的数据
+                    // 入库的数据
+                    if (!this.pNode.childOrganizations) {
+                        this.$set(this.pNode, 'childOrganizations', [])
+                    }
+                    this.pNode.disabled = true
+                    this.pNode.childOrganizations.push(newChild)
+                    params.createBy = this.userInfo.employeeName
+                    params.parentOrganizationCode = this.pNode.organizationCode
+                    // await updateOrganizationNode(params)
+                } else {
+                    params.updateBy = this.userInfo.employeeName
+                    params.organizationCode = this.pNode.organizationCode
+                    this.$set(this.pNode, 'organizationName', this.form.name)
+                    // await editOrganizationNode(params)
+                }
+                this.dialogVisible = false
+            }
+        })
     }
 
     private async findBossTree () {
-        const { data } = await findOrganizationTree()
+        const { data } = await findOrganizationTree(1)
+        this.bossList = this.onCheckBossLastStage(data.organizationNodeList)
     }
 
     private async findEhrTree () {
         const { data } = await findEhrTree()
-        this.data1 = this.onCheckLastStage(data.departmentNodeVOS)
-        this.data = this.onCheckLastStage(data.departmentNodeVOS)
+        this.ehrList = this.onCheckEhrLastStage(data.departmentNodeVOS)
+        console.log(this.ehrList)
     }
 
     mounted () {
