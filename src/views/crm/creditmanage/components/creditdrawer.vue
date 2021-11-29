ed88394e31c10e56465d50f789c34b609057ebc2
jili <jili52012@126.com>
2021-11-29 10:48:37 +0800

    弹层问题

--- src/views/crm/creditmanage/components/creditdrawer.vue
+++ src/views/crm/creditmanage/components/creditdrawer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="projectRecord">
-        <h-drawer title="信用详情" :visible.sync="drawer" :before-close="handleClose" modal-append-to-body :wrapperClosable=false size="50%">
+        <h-drawer title="信用详情" :visible.sync="drawer" :before-close="handleClose" :modal-append-to-body="true" :wrapperClosable=false size="50%">
             <template #connect>
                 <el-tabs v-model="activeName" @tab-click="handleClick" type="card" class="fiextab">
                     <el-tab-pane label="信用详情" name="1"></el-tab-pane>
