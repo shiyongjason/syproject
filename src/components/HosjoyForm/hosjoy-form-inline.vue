d39513dc25108cb856da0b59c611fff721e9a785
sunjun <990375133@qq.com>
2022-02-07 09:30:31 +0800

    测试

---
+++ src/components/HosjoyForm/hosjoy-form-inline.vue
@@ -0,0 +1,53 @@
+<template>
+        <div class="agel-form-inline">
+            <template v-for="(row,rowIndex) in dynamicData">
+                <slot name="prepend" v-bind="{row,rowIndex}"></slot>
+                <agel-form-item
+                    v-for="(item,colIndex) in agItems"
+                    :key="row._key_+item.prop"
+                    v-show="item.show"
+                    v-bind="getFormItemAttrs({item,colIndex,row,rowIndex})"
+                    :component="getComponentAttrs({item,colIndex,row,rowIndex})"
+                    />
+                <slot name="append" v-bind="{row,rowIndex}"></slot>
+            </template>
+        </div>
+</template>
+
+<script>
+import itemsMinxin from './itemsMixin'
+import agelFormItem from './agel-form-item.vue'
+export default {
+    // 行内表单
+    name: 'hosjoy-form-inline',
+    mixins: [itemsMinxin],
+    inheritAttrs: false,
+    components: {
+        agelFormItem
+    },
+    mounted () {
+        console.log('log::::::this.agItems', this.agItems)
+    }
+}
+</script>
+
+ <style>
+.agel-form-inline {
+  display: inline-block;
+}
+
+.agel-form-inline .el-form-item {
+  display: inline-block;
+  margin-right: 10px;
+  vertical-align: top;
+}
+
+.agel-form-inline .el-form-item__label-wrap {
+  margin-left: 0px !important;
+}
+
+.agel-form-inline .el-form-item__content {
+  display: inline-block;
+  margin-left: 0px !important;
+}
+</style>
