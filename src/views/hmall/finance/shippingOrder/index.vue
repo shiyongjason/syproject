9f31898102ee77bde05df1f87c05c1b106fc00a1
sunjun <990375133@qq.com>
2022-02-10 17:37:34 +0800

    BTB-37596 【BOSS】运费订单，修改运费弹框，未显示运费金额

--- src/views/hmall/finance/shippingOrder/index.vue
+++ src/views/hmall/finance/shippingOrder/index.vue
@@ -110,7 +110,7 @@
                     <div class="freightpopover" v-if="scope.data.row.status<=10">
                         <el-popover placement="left" width='200' :ref="`popover-${scope.data.$index}`" trigger="click" title='运费修改'
                          @show="onPopoverShow">
-                            <p>运费金额：{{typeof scope.data.row.totalAmount === 'number'?`￥${scope.data.row.totalAmount}`:'-'}}</p>
+                            <p>运费金额：{{typeof scope.data.row.finalTotalAmount === 'number'?`￥${scope.data.row.finalTotalAmount}`:'-'}}</p>
                             <p class="amountinput"><el-input v-model="amount" v-isNum:2 v-inputMAX='100000' maxlength="50" placeholder="请输入修改的运费金额" size="mini"></el-input></p>
                             <div style="text-align: right; margin: 0">
                                 <el-button size="mini" type="text" @click="()=>closeFreightVisible(scope.data.$index)">取消</el-button>
