beaf6b0db946250d70664ade910363f9eb68e6c6
shiyong <422703012@qq.com>
2022-03-15 10:13:55 +0800

    作废

--- src/views/crm/supplierSearch/api/index.js
+++ src/views/crm/supplierSearch/api/index.js
@@ -2,3 +2,7 @@ import axios from 'axios'
 
 // 供应商查询
 export const getSupplierSearchList = (params) => axios.get('/project/api/supplier-accounts/page', { params })
+// 账号作废
+export const updateInvalidNo = (params) => axios.patch(`/project/api/supplier-accounts/${params}/invalid`)
+// 账号取消作废
+export const updateValidNo = (params) => axios.patch(`/project/api/supplier-accounts/${params}/valid`)
